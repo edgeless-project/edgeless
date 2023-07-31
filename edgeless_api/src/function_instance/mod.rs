4ee4312328e3f68bb042a3826c4e9b28c2560be4
Raphael Hetzel <hetzel@in.tum.de>
2023-07-31 13:41:04 +0000

    Multi Resource Configuration Proxy

--- edgeless_api/src/function_instance/mod.rs
+++ edgeless_api/src/function_instance/mod.rs
@@ -1,4 +1,4 @@
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct FunctionId {
     pub node_id: uuid::Uuid,
     pub function_id: uuid::Uuid,
--- edgeless_api/src/resource_configuration/mod.rs
+++ edgeless_api/src/resource_configuration/mod.rs
@@ -18,3 +18,43 @@ pub trait ResourceConfigurationAPI: Sync + Send {
     ) -> anyhow::Result<crate::function_instance::FunctionId>;
     async fn stop_resource_instance(&mut self, resource_id: crate::function_instance::FunctionId) -> anyhow::Result<()>;
 }
+
+pub struct MultiResouceConfigurationAPI {
+    pub resource_providers: std::collections::HashMap<String, Box<dyn ResourceConfigurationAPI>>,
+    pub resouce_instances: std::collections::HashMap<crate::function_instance::FunctionId, String>,
+}
+
+impl MultiResouceConfigurationAPI {
+    pub fn new(resource_providers: std::collections::HashMap<String, Box<dyn ResourceConfigurationAPI>>) -> Self {
+        Self {
+            resource_providers,
+            resouce_instances: std::collections::HashMap::new(),
+        }
+    }
+}
+
+#[async_trait::async_trait]
+impl ResourceConfigurationAPI for MultiResouceConfigurationAPI {
+    async fn start_resource_instance(
+        &mut self,
+        instance_specification: ResourceInstanceSpecification,
+    ) -> anyhow::Result<crate::function_instance::FunctionId> {
+        if let Some(resource) = self.resource_providers.get_mut(&instance_specification.provider_id) {
+            let provider = instance_specification.provider_id.clone();
+            let id = resource.start_resource_instance(instance_specification).await?;
+            self.resouce_instances.insert(id.clone(), provider.clone());
+            Ok(id)
+        } else {
+            return Err(anyhow::anyhow!("Resource provider does not exist"));
+        }
+    }
+
+    async fn stop_resource_instance(&mut self, resource_id: crate::function_instance::FunctionId) -> anyhow::Result<()> {
+        if let Some(instance_id) = self.resouce_instances.get(&resource_id) {
+            if let Some(provider) = self.resource_providers.get_mut(instance_id) {
+                return provider.stop_resource_instance(resource_id).await;
+            }
+        }
+        Err(anyhow::anyhow!("Could not delete. (Missing?)"))
+    }
+}
