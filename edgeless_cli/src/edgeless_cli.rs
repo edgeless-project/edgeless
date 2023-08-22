2a9f1b774bb1b4ce63adf2a7d6cb3ec7f825b4e3
Claudio Cicconetti <c.cicconetti@iit.cnr.it>
2023-08-22 16:39:33 +0200

    cli minor change: only create an API client with workflow commands

--- edgeless_cli/src/edgeless_cli.rs
+++ edgeless_cli/src/edgeless_cli.rs
@@ -56,12 +56,12 @@ async fn main() -> anyhow::Result<()> {
 
     match args.command {
         None => println!("Bye"),
-        Some(x) => {
-            let conf: CLiConfig = toml::from_str(&std::fs::read_to_string(args.config_file)?)?;
-            let mut con_client = edgeless_api::grpc_impl::controller::ControllerAPIClient::new(&conf.controller_url).await;
-            let mut con_wf_client = con_client.workflow_instance_api();
-            match x {
-                Commands::Workflow { workflow_command } => match workflow_command {
+        Some(x) => match x {
+            Commands::Workflow { workflow_command } => {
+                let conf: CLiConfig = toml::from_str(&std::fs::read_to_string(args.config_file)?)?;
+                let mut con_client = edgeless_api::grpc_impl::controller::ControllerAPIClient::new(&conf.controller_url).await;
+                let mut con_wf_client = con_client.workflow_instance_api();
+                match workflow_command {
                     WorkflowCommands::Start { spec_file } => {
                         let workflow: workflow_spec::WorkflowSpec = serde_json::from_str(&std::fs::read_to_string(spec_file.clone())?)?;
                         let my_wf_id = edgeless_api::workflow_instance::WorkflowId {
@@ -119,76 +119,75 @@ async fn main() -> anyhow::Result<()> {
                             println!("Workflow Stopped");
                         }
                     }
-                },
-                Commands::Function { function_command } => match function_command {
-                    FunctionCommands::Build { spec_file } => {
-                        let spec_file_path = std::fs::canonicalize(std::path::PathBuf::from(spec_file.clone()))?;
-                        let cargo_project_path = spec_file_path.parent().unwrap().to_path_buf();
-                        let cargo_manifest = cargo_project_path.join("Cargo.toml");
-
-                        let function_spec: workflow_spec::WorkflowSpecFunctionClass =
-                            serde_json::from_str(&std::fs::read_to_string(spec_file.clone())?)?;
-                        let build_dir = std::env::temp_dir().join(format!("edgeless-{}-{}", function_spec.id, uuid::Uuid::new_v4()));
-
-                        let config = &cargo::util::config::Config::default()?;
-                        let mut ws = cargo::core::Workspace::new(&cargo_manifest, config.clone())?;
-                        ws.set_target_dir(cargo::util::Filesystem::new(build_dir.clone()));
-
-                        let pack = ws.current()?;
-
-                        let lib_name = match pack.library() {
-                            Some(val) => val.name(),
-                            None => {
-                                return Err(anyhow::anyhow!("Cargo package does not contain library."));
-                            }
-                        };
-
-                        let mut build_config = cargo::core::compiler::BuildConfig::new(
-                            config,
-                            None,
-                            false,
-                            &vec!["wasm32-unknown-unknown".to_string()],
-                            cargo::core::compiler::CompileMode::Build,
-                        )?;
-                        build_config.requested_profile = cargo::util::interning::InternedString::new("release");
-
-                        let compile_options = cargo::ops::CompileOptions {
-                            build_config: build_config,
-                            cli_features: cargo::core::resolver::CliFeatures::new_all(false),
-                            spec: cargo::ops::Packages::Packages(Vec::new()),
-                            filter: cargo::ops::CompileFilter::Default {
-                                required_features_filterable: false,
-                            },
-                            target_rustdoc_args: None,
-                            target_rustc_args: None,
-                            target_rustc_crate_types: None,
-                            rustdoc_document_private_items: false,
-                            honor_rust_version: true,
-                        };
-
-                        cargo::ops::compile(&ws, &compile_options)?;
-
-                        let raw_result = build_dir
-                            .join(format!("wasm32-unknown-unknown/release/{}.wasm", lib_name))
-                            .to_str()
-                            .unwrap()
-                            .to_string();
-                        let out_file = cargo_project_path
-                            .join(format!("{}.wasm", function_spec.id))
-                            .to_str()
-                            .unwrap()
-                            .to_string();
-
-                        println!(
-                            "{:?}",
-                            std::process::Command::new("wasm-tools")
-                                .args(["component", "new", &raw_result, "-o", &out_file])
-                                .status()?
-                        );
-                    }
-                },
+                }
             }
-        }
+            Commands::Function { function_command } => match function_command {
+                FunctionCommands::Build { spec_file } => {
+                    let spec_file_path = std::fs::canonicalize(std::path::PathBuf::from(spec_file.clone()))?;
+                    let cargo_project_path = spec_file_path.parent().unwrap().to_path_buf();
+                    let cargo_manifest = cargo_project_path.join("Cargo.toml");
+
+                    let function_spec: workflow_spec::WorkflowSpecFunctionClass = serde_json::from_str(&std::fs::read_to_string(spec_file.clone())?)?;
+                    let build_dir = std::env::temp_dir().join(format!("edgeless-{}-{}", function_spec.id, uuid::Uuid::new_v4()));
+
+                    let config = &cargo::util::config::Config::default()?;
+                    let mut ws = cargo::core::Workspace::new(&cargo_manifest, config.clone())?;
+                    ws.set_target_dir(cargo::util::Filesystem::new(build_dir.clone()));
+
+                    let pack = ws.current()?;
+
+                    let lib_name = match pack.library() {
+                        Some(val) => val.name(),
+                        None => {
+                            return Err(anyhow::anyhow!("Cargo package does not contain library."));
+                        }
+                    };
+
+                    let mut build_config = cargo::core::compiler::BuildConfig::new(
+                        config,
+                        None,
+                        false,
+                        &vec!["wasm32-unknown-unknown".to_string()],
+                        cargo::core::compiler::CompileMode::Build,
+                    )?;
+                    build_config.requested_profile = cargo::util::interning::InternedString::new("release");
+
+                    let compile_options = cargo::ops::CompileOptions {
+                        build_config: build_config,
+                        cli_features: cargo::core::resolver::CliFeatures::new_all(false),
+                        spec: cargo::ops::Packages::Packages(Vec::new()),
+                        filter: cargo::ops::CompileFilter::Default {
+                            required_features_filterable: false,
+                        },
+                        target_rustdoc_args: None,
+                        target_rustc_args: None,
+                        target_rustc_crate_types: None,
+                        rustdoc_document_private_items: false,
+                        honor_rust_version: true,
+                    };
+
+                    cargo::ops::compile(&ws, &compile_options)?;
+
+                    let raw_result = build_dir
+                        .join(format!("wasm32-unknown-unknown/release/{}.wasm", lib_name))
+                        .to_str()
+                        .unwrap()
+                        .to_string();
+                    let out_file = cargo_project_path
+                        .join(format!("{}.wasm", function_spec.id))
+                        .to_str()
+                        .unwrap()
+                        .to_string();
+
+                    println!(
+                        "{:?}",
+                        std::process::Command::new("wasm-tools")
+                            .args(["component", "new", &raw_result, "-o", &out_file])
+                            .status()?
+                    );
+                }
+            },
+        },
     }
     Ok(())
 }
