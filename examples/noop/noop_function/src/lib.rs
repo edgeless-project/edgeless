e025de4d06a7eaa9ed408443186334e9da1c32c9
Claudio Cicconetti <c.cicconetti@iit.cnr.it>
2023-08-22 16:39:48 +0200

    Add noop example function

---
+++ examples/noop/noop_function/src/lib.rs
@@ -0,0 +1,25 @@
+use edgeless_function::api::*;
+use log;
+
+struct NoopFunction;
+
+impl Edgefunction for NoopFunction {
+    fn handle_cast(src: Fid, encoded_message: String) {
+        log::info!("Noop casted, node {}, function {}, MSG: {}", src.node, src.function, encoded_message);
+    }
+
+    fn handle_call(src: Fid, encoded_message: String) -> CallRet {
+        log::info!("Noop called, node {}, function {}, MSG: {}", src.node, src.function, encoded_message);
+        CallRet::Noreply
+    }
+
+    fn handle_init(payload: String, _serialized_state: Option<String>) {
+        log::info!("Noop initialized, payload: {}", payload);
+    }
+
+    fn handle_stop() {
+        log::info!("Noop stopped");
+    }
+}
+
+edgeless_function::export!(NoopFunction);
