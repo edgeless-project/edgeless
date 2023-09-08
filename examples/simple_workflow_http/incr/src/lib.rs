f4f9a71dc1db272b67025d11c8d555d2ea60ba6d
Claudio Cicconetti <c.cicconetti@iit.cnr.it>
2023-09-08 15:09:10 +0200

    Add an example of HTTP ingress/egress with intermediate functions

---
+++ examples/simple_workflow_http/incr/src/lib.rs
@@ -0,0 +1,28 @@
+use edgeless_function::api::*;
+
+struct IncrFun;
+
+impl Edgefunction for IncrFun {
+    fn handle_cast(_src: InstanceId, encoded_message: String) {
+        log::info!("incr: called with '{}'", encoded_message);
+
+        if let Ok(n) = encoded_message.parse::<i32>() {
+            cast_alias("cb_success", format!("{}", n + 1).as_str());
+        }
+    }
+
+    fn handle_call(_src: InstanceId, _encoded_message: String) -> CallRet {
+        CallRet::Noreply
+    }
+
+    fn handle_init(_payload: String, _init_state: Option<String>) {
+        edgeless_function::init_logger();
+        log::info!("incr: started");
+    }
+
+    fn handle_stop() {
+        log::info!("incr: stopped");
+    }
+}
+
+edgeless_function::export!(IncrFun);
