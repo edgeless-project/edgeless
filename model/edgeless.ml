f5218c7d9ed69cb8df2251ae9c14a7f706246fb7
Richard Mortier <mort@cantab.net>
2023-09-28 10:51:12 +0200

    model: add Instance.t

--- model/edgeless.ml
+++ model/edgeless.ml
@@ -8,14 +8,15 @@ type alias = string (* Naming things *)
 
 (** Forwarding tables.
 
-A forwarding table is a list of forwarding table entries, each of which
-maps a specific function instance in a specific workflow to one or more
+A forwarding table is a list of forwarding table entries assoicated with
+a function Instance, each of which maps a specific output to one or more
 function instances on other nodes with some priority for load balancing.
 *)
 
 type prio = int (* Entry priority, for load balancing *)
+type output = string (* Output channel for a function *)
 type target = nid * fid (* Target for a function output *)
-type fwde = (wid * fid) * (target * prio) list
+type fwde = output * (target * prio) list
 (* Forwarding table entries, the list of postential destination function
    instances with associated priorities corresponding to a function i
    nstance on this node *)
@@ -54,23 +55,28 @@ end = struct
   type t = { repository : rid; alias : alias; runtime : runtime }
 end
 
+(** Function instance.
+
+A function instance is a running function hosted on a node, with a
+forwarding table attached.
+*)
+
+module Instance : sig
+  type t
+end = struct
+  type t = { fid : fid; func : Function.t; fwdt : fwdt }
+end
 
 (** Function invocations.
 
 A function invocation is a named stage in a Workflow that may invoke one
-or more output callbacks.
-
+or more outputs.
 *)
 module Invocation : sig
   type t
 end = struct
-  type t = {
-    alias : alias;
-    func : Function.t;
-    outputs : Function.t list;
-  }
+  type t = { alias : alias; func : Function.t; outputs : output list }
 end
-
 
 (** Workflows.
     
@@ -80,10 +86,7 @@ A workflow is a named list of function invocations, representing a DAG.
 module Workflow : sig
   type t
 end = struct
-  type t = {
-    alias: string;
-    functions: Invocation.t list;
-  }
+  type t = { alias : string; functions : Invocation.t list }
 end
 
 (** Nodes.
@@ -97,9 +100,10 @@ module Node : sig
   type t
 end = struct
   type t = {
+    nid : nid;
     resources : cpu * memory * resource list;
     runtime : runtime;
-    fwdt : fwdt;
+    instances : Instance.t list;
   }
 end
 
