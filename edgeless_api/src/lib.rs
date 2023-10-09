94e9c8b63a9bccf831b1adfeabb93cb48267e85a
Raphael Hetzel <hetzel@in.tum.de>
2023-10-09 20:01:01 +0000

    Initial Edgeless-Embedded Implementation

--- edgeless_api/src/lib.rs
+++ edgeless_api/src/lib.rs
@@ -1,20 +1,22 @@
+#[cfg(feature = "grpc_impl")]
+pub mod grpc_impl;
+
+pub mod coap_impl;
+
+pub mod function_instance;
+
+pub mod workflow_instance;
+
+pub mod resource_configuration;
+
 pub mod agent;
 
 pub mod common;
 
 pub mod controller;
 
-pub mod function_instance;
-
-#[cfg(feature = "grpc_impl")]
-pub mod grpc_impl;
-
 pub mod invocation;
 
 pub mod orc;
 
-pub mod resource_configuration;
-
 pub mod util;
-
-pub mod workflow_instance;
--- edgeless_embedded_esp32/src/main.rs
+++ edgeless_embedded_esp32/src/main.rs
@@ -3,17 +3,42 @@
 #![no_std]
 #![no_main]
 #![feature(type_alias_impl_trait)]
+#![feature(async_fn_in_trait)]
 
+extern crate alloc;
+
+pub mod agent;
+pub mod coap;
+pub mod dataplane;
+pub mod epaper_display;
+pub mod mock_display;
+pub mod mock_sensor;
+pub mod resource;
+pub mod scd30_sensor;
 pub mod wifi;
 
+use epd_waveshare::prelude::WaveshareDisplay;
 use esp_backtrace as _;
-
-use hal::{
-    prelude::*,
-};
+use hal::prelude::*;
 
 #[global_allocator]
 static ALLOCATOR: esp_alloc::EspHeap = esp_alloc::EspHeap::empty();
+
+static RNG: once_cell::sync::OnceCell<hal::Rng> = once_cell::sync::OnceCell::new();
+
+const ESP_GETRANDOM_ERROR: u32 = getrandom::Error::CUSTOM_START + 1;
+
+const NODE_ID: uuid::Uuid = uuid::uuid!("0827240a-3050-4604-bf3e-564c41c77106");
+
+const COAP_PEERS: [(uuid::Uuid, smoltcp::wire::IpEndpoint); 1] = [(
+    uuid::uuid!("fda6ce79-46df-4f96-a0d2-456f720f606c"),
+    smoltcp::wire::IpEndpoint {
+        addr: embassy_net::IpAddress::v4(192, 168, 2, 61),
+        port: 7002,
+    },
+)];
+
+use crate::epaper_display::EPaper;
 
 fn init_heap() {
     const HEAP_SIZE: usize = 32 * 1024;
@@ -24,37 +49,165 @@ fn init_heap() {
     }
 }
 
+fn esp_getrandom(dest: &mut [u8]) -> Result<(), getrandom::Error> {
+    match RNG.get() {
+        Some(rng) => {
+            let mut rng = rng.clone();
+            for dest_byte in dest {
+                *dest_byte = rng.random() as u8;
+            }
+            Ok(())
+        }
+        None => Err(getrandom::Error::from(core::num::NonZeroU32::new(ESP_GETRANDOM_ERROR).unwrap())),
+    }
+}
+
+getrandom::register_custom_getrandom!(esp_getrandom);
+
 #[entry]
 fn main() -> ! {
     esp_println::logger::init_logger_from_env();
-    log::info!("Start Edgeless Embedded.");
-    
+    esp_println::println!("Start Edgeless Embedded.");
+
     // https://github.com/esp-rs/esp-template/blob/main/src/main.rs
     init_heap();
-    
+
     let peripherals = hal::peripherals::Peripherals::take();
-    let mut system = peripherals.DPORT.split();
-    
-    let clocks =  hal::clock::ClockControl::max(system.clock_control).freeze();
-    let timer_group0 = hal::timer::TimerGroup::new(
-        peripherals.TIMG0,
-        &clocks,
-        &mut system.peripheral_clock_control,
-    );
-    let timer_group1 = hal::timer::TimerGroup::new(
-        peripherals.TIMG1,
-        &clocks,
-        &mut system.peripheral_clock_control,
-    );
+    let io = hal::IO::new(peripherals.GPIO, peripherals.IO_MUX);
+    let system = peripherals.SYSTEM.split();
+
+    let clocks = hal::clock::ClockControl::max(system.clock_control).freeze();
+    let timer_group0 = hal::timer::TimerGroup::new(peripherals.TIMG0, &clocks);
+    let timer_group1 = hal::timer::TimerGroup::new(peripherals.TIMG1, &clocks);
 
     let rng = hal::Rng::new(peripherals.RNG);
+    assert!(RNG.set(rng.clone()).is_ok());
 
     hal::embassy::init(&clocks, timer_group0.timer0);
 
+    let spi = static_cell::make_static!(hal::spi::SpiBusController::from_spi(hal::spi::Spi::new_no_cs_no_miso(
+        peripherals.SPI2,
+        io.pins.gpio18,
+        io.pins.gpio23,
+        100u32.kHz(),
+        hal::spi::SpiMode::Mode0,
+        &clocks
+    )));
+
+    let i2c = hal::i2c::I2C::new(peripherals.I2C0, io.pins.gpio33, io.pins.gpio32, 50u32.kHz(), &clocks);
+
+    let mut i2c_delay = hal::delay::Delay::new(&clocks);
+    i2c_delay.delay_ms(2000u32);
+
+    let scd = sensor_scd30::Scd30::new(i2c, i2c_delay).unwrap();
+
+    let scd30 = static_cell::make_static!(scd30_sensor::SCD30SensorWrapper { sensor: scd });
+
+    let mut spi_dev = spi.add_device(io.pins.gpio5);
+    let busy_pin = io.pins.gpio4.into_floating_input();
+    let dc_pin = io.pins.gpio17.into_push_pull_output();
+    let rst_pin = io.pins.gpio16.into_push_pull_output();
+    let mut epaper_delay = hal::delay::Delay::new(&clocks);
+
     let executor = static_cell::make_static!(hal::embassy::executor::Executor::new());
 
-    executor.run(|spawner| {
-        spawner.spawn(wifi::init(spawner.clone(), timer_group1.timer0, rng, system.radio_clock_control, clocks, peripherals.RADIO)).unwrap();
+    let epd = epd_waveshare::epd2in13_lillygo::Epd2in13::new(&mut spi_dev, busy_pin, dc_pin, rst_pin, &mut epaper_delay, None).unwrap();
+
+    let display = epd_waveshare::epd2in13_lillygo::Display2in13::default();
+
+    let display = static_cell::make_static!(epaper_display::LillyGoEPaper {
+        spi_dev: spi_dev,
+        epd: epd,
+        display: display,
+        delay: epaper_delay
     });
 
+    display.set_text("Edgeless");
+
+    executor.run(|spawner| {
+        spawner.spawn(edgeless(
+            spawner,
+            timer_group1.timer0,
+            rng,
+            system.radio_clock_control,
+            clocks,
+            peripherals.RADIO,
+            display,
+            scd30,
+        ));
+    });
+
+    loop {}
+}
+
+#[embassy_executor::task]
+async fn edgeless(
+    spawner: embassy_executor::Spawner,
+    timer: esp_wifi::EspWifiTimer,
+    rng: hal::Rng,
+    radio_clock_control: hal::system::RadioClockControl,
+    clocks: hal::clock::Clocks<'static>,
+    radio: hal::peripherals::RADIO,
+    display: &'static mut dyn epaper_display::EPaper,
+    scd30: &'static mut dyn scd30_sensor::Sensor,
+) {
+    log::info!("Edgeless Embedded Async Main");
+
+    let stack = wifi::init(spawner.clone(), timer, rng, radio_clock_control, clocks, radio).await;
+
+    let channel = static_cell::make_static!(embassy_sync::channel::Channel::<
+        embassy_sync::blocking_mutex::raw::NoopRawMutex,
+        edgeless_api_core::invocation::Event<heapless::String<1500>>,
+        2,
+    >::new());
+    let sender = channel.sender();
+    let receiver = channel.receiver();
+    let node_id = NODE_ID.clone();
+
+    let mock_sensor_state = static_cell::make_static!(core::cell::RefCell::new(embassy_sync::mutex::Mutex::new(mock_sensor::MockSensorInner {
+        instance_id: None,
+        data_out_id: None,
+        delay: 30
+    })));
+
+    let sensor_state = static_cell::make_static!(core::cell::RefCell::new(embassy_sync::mutex::Mutex::new(
+        scd30_sensor::SCD30SensorInner {
+            instance_id: None,
+            data_out_id: None,
+            delay: 5,
+            sensor: scd30
+        }
+    )));
+
+    let resource_registry = static_cell::make_static!(agent::ResourceRegistry {
+        own_node_id: node_id.clone(),
+        upstream_sender: sender,
+        inner: static_cell::make_static!(core::cell::RefCell::new(embassy_sync::mutex::Mutex::new(agent::ResourceRegistryInner {
+            mock_display: (mock_display::MockDisplay {
+                instance_id: None,
+                active: false
+            }),
+            display: (epaper_display::EPaperDisplay {
+                header: None,
+                instance_id: None,
+                display: display
+            }),
+            mock_sensor: (mock_sensor::MockSensor { inner: mock_sensor_state }),
+            sensor: (scd30_sensor::SCD30Sensor { inner: sensor_state })
+        })))
+    });
+
+    spawner.spawn(coap::coap_task(stack, receiver, resource_registry.clone()));
+
+    let dataplane_handle = dataplane::EmbeddedDataplaneHandle {
+        reg: resource_registry.clone(),
+    };
+
+    spawner.spawn(mock_sensor::mock_sensor_task(mock_sensor_state, dataplane_handle));
+
+    let dataplane_handle = dataplane::EmbeddedDataplaneHandle {
+        reg: resource_registry.clone(),
+    };
+
+    spawner.spawn(scd30_sensor::scd30_sensor_task(sensor_state, dataplane_handle));
 }
