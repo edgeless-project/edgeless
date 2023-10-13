fa4e391ab2814f03540f0488ca675b886bc411e9
Claudio Cicconetti <c.cicconetti@iit.cnr.it>
2023-10-13 15:48:49 +0200

    Implement new resource type: redis

--- edgeless_con/src/lib.rs
+++ edgeless_con/src/lib.rs
@@ -40,9 +40,10 @@ orchestrators = [
     { domain_id = "domain-1", orchestrator_url="http://127.0.0.1:7011" }
 ]
 resources = [
-    { resource_provider_id = "file-log-1",  resource_class_type = "file-log", output_callback_declarations = [], resource_configuration_url = "http://127.0.0.1:7033" },
-    { resource_provider_id = "http-ingress-1",  resource_class_type = "http-ingress", output_callback_declarations = ["new_request"], resource_configuration_url = "http://127.0.0.1:7033" },
-    { resource_provider_id = "http-egress-1",  resource_class_type = "http-egress", output_callback_declarations = [], resource_configuration_url = "http://127.0.0.1:7033" }
+    { resource_provider_id = "file-log-1",     resource_class_type = "file-log",     output_callback_declarations = [],              resource_configuration_url = "http://127.0.0.1:7033" },
+    { resource_provider_id = "http-ingress-1", resource_class_type = "http-ingress", output_callback_declarations = ["new_request"], resource_configuration_url = "http://127.0.0.1:7033" },
+    { resource_provider_id = "http-egress-1",  resource_class_type = "http-egress",  output_callback_declarations = [],              resource_configuration_url = "http://127.0.0.1:7033" },
+    { resource_provider_id = "redis-1",        resource_class_type = "redis",        output_callback_declarations = [],              resource_configuration_url = "http://127.0.0.1:7033" }
 ]
 "##,
     )
---
+++ examples/redis/counter/src/lib.rs
@@ -0,0 +1,31 @@
+use edgeless_function::api::*;
+
+struct Counter;
+
+impl Edgefunction for Counter {
+    fn handle_cast(_src: InstanceId, message: String) {
+        let prev_count = message.parse::<i32>().unwrap();
+        let cur_count = format!("{}", prev_count + 1);
+        cast_alias("output", cur_count.as_str());
+        delayed_cast(1000, &slf(), &cur_count);
+    }
+
+    fn handle_call(_src: InstanceId, _message: String) -> CallRet {
+        CallRet::Noreply
+    }
+
+    fn handle_init(init_message: String, _serialized_state: Option<String>) {
+        edgeless_function::init_logger();
+        let message = match init_message.parse::<i32>() {
+            Ok(_) => init_message,
+            Err(_) => "0".to_string(),
+        };
+        cast(&slf(), &message);
+    }
+
+    fn handle_stop() {
+        // noop
+    }
+}
+
+edgeless_function::export!(Counter);
