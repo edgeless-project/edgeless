0f665f4f8e1795ff69addeee1390d86487c191f5
Claudio Cicconetti <c.cicconetti@iit.cnr.it>
2023-10-18 11:36:34 +0200

    Update make_example.sh script

--- scripts/make_example.sh
+++ scripts/make_example.sh
@@ -22,15 +22,18 @@ fi
 
 function_all_lowercase=${function_name,,}
 function_first_uppercase=${function_all_lowercase^}
+function_camel_case=$(echo $function_all_lowercase | sed -r 's/(^|_)(\w)/\U\2/g')
 
 mkdir -p examples/$workflow_name
 cd examples/$workflow_name
 tar xf ../../scripts/example_template.tgz
 mv SMALLNAME_function ${function_all_lowercase}_function
+
 sed -i -e "s/AUTHOR/${author_name}/g" $(rgrep AUTHOR | cut -f 1 -d : | sort -u)
 sed -i -e "s/WORKFLOWNAME/${workflow_name}/g" $(rgrep WORKFLOWNAME | cut -f 1 -d : | sort -u)
 sed -i -e "s/SMALLNAME/${function_all_lowercase}/g" $(rgrep SMALLNAME | cut -f 1 -d : | sort -u)
 sed -i -e "s/BIGNAME/${function_first_uppercase}/g" $(rgrep BIGNAME | cut -f 1 -d : | sort -u)
+sed -i -e "s/CAMELCASENAME/${function_camel_case}/g" $(rgrep CAMELCASENAME | cut -f 1 -d : | sort -u)
 cd ../../
 
 echo "the template is ready to be used in: examples/$workflow_name"
