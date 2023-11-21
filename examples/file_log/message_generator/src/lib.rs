126c4fdc6aa2ef0770481b92c0e378df1351a2d5
Claudio Cicconetti <c.cicconetti@iit.cnr.it>
2023-11-21 12:18:02 +0100

    Update WASM programming interface
    delayed-cast-raw -> delayed-cast (using an alias instead of an InstanceId)
    
    call/cast methods now accept "self" to refer to this function instance (this makes slf() obsolete)

--- examples/file_log/message_generator/src/lib.rs
+++ examples/file_log/message_generator/src/lib.rs
@@ -5,7 +5,7 @@ struct MessageGenerator;
 impl Edgefunction for MessageGenerator {
     fn handle_cast(src: InstanceId, message: String) {
         cast("output", format!("{} from {}:{}", &message, src.node, src.function).as_str());
-        delayed_cast_raw(1000, &slf(), &message);
+        delayed_cast(1000, "self", &message);
     }
 
     fn handle_call(_src: InstanceId, _message: String) -> CallRet {
@@ -14,7 +14,7 @@ impl Edgefunction for MessageGenerator {
 
     fn handle_init(init_message: String, _serialized_state: Option<String>) {
         edgeless_function::init_logger();
-        cast_raw(&slf(), &init_message);
+        cast("self", &init_message);
     }
 
     fn handle_stop() {
