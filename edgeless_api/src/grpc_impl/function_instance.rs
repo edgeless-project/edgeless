83a53489f63afa6d928d48cd68003d7712f1894d
Claudio Cicconetti <c.cicconetti@iit.cnr.it>
2023-12-27 12:10:02 +0100

    WIP: update FunctionInstanceOrc API
    Use newly-defined message StartResourceRequest in StartResource().

--- edgeless_api/src/grpc_impl/function_instance.rs
+++ edgeless_api/src/grpc_impl/function_instance.rs
@@ -41,6 +41,15 @@ impl FunctonInstanceConverters {
         })
     }
 
+    pub fn parse_start_resource_request(
+        api_workflow: &crate::grpc_impl::api::StartResourceRequest,
+    ) -> anyhow::Result<crate::function_instance::StartResourceRequest> {
+        Ok(crate::function_instance::StartResourceRequest {
+            class_type: api_workflow.class_type.clone(),
+            configurations: api_workflow.configurations.clone(),
+        })
+    }
+
     pub fn parse_update_node_request(
         api_instance: &crate::grpc_impl::api::UpdateNodeRequest,
     ) -> anyhow::Result<crate::function_instance::UpdateNodeRequest> {
@@ -212,6 +221,15 @@ impl FunctonInstanceConverters {
         }
     }
 
+    pub fn serialize_start_resource_request(
+        crate_resource: &crate::function_instance::StartResourceRequest,
+    ) -> crate::grpc_impl::api::StartResourceRequest {
+        crate::grpc_impl::api::StartResourceRequest {
+            class_type: crate_resource.class_type.clone(),
+            configurations: crate_resource.configurations.clone(),
+        }
+    }
+
     pub fn serialize_update_node_request(req: &crate::function_instance::UpdateNodeRequest) -> crate::grpc_impl::api::UpdateNodeRequest {
         match req {
             crate::function_instance::UpdateNodeRequest::Registration(node_id, agent_url, invocation_url, resource_providers) => {
@@ -368,10 +386,10 @@ impl crate::function_instance::FunctionInstanceOrcAPI for FunctionInstanceOrcAPI
 
     async fn start_resource(
         &mut self,
-        instance_specification: crate::workflow_instance::WorkflowResource,
+        instance_specification: crate::function_instance::StartResourceRequest,
     ) -> anyhow::Result<crate::common::StartComponentResponse> {
         let serialized_request =
-            crate::grpc_impl::workflow_instance::WorkflowInstanceConverters::serialize_workflow_resource(&instance_specification);
+            crate::grpc_impl::function_instance::FunctonInstanceConverters::serialize_start_resource_request(&instance_specification);
         match self.client.start_resource(tonic::Request::new(serialized_request)).await {
             Ok(ret) => crate::grpc_impl::common::CommonConverters::parse_start_component_response(&ret.into_inner()),
             Err(err) => Err(anyhow::anyhow!("Resource configuration request failed: {}", err)),
@@ -473,10 +491,10 @@ impl crate::grpc_impl::api::function_instance_orc_server::FunctionInstanceOrc fo
 
     async fn start_resource(
         &self,
-        request: tonic::Request<crate::grpc_impl::api::WorkflowResource>,
+        request: tonic::Request<crate::grpc_impl::api::StartResourceRequest>,
     ) -> tonic::Result<tonic::Response<crate::grpc_impl::api::StartComponentResponse>> {
         let inner = request.into_inner();
-        let parsed_spec = match crate::grpc_impl::workflow_instance::WorkflowInstanceConverters::parse_workflow_resource(&inner) {
+        let parsed_spec = match crate::grpc_impl::function_instance::FunctonInstanceConverters::parse_start_resource_request(&inner) {
             Ok(val) => val,
             Err(err) => {
                 return Ok(tonic::Response::new(crate::grpc_impl::api::StartComponentResponse {
@@ -753,6 +771,7 @@ mod tests {
     use crate::function_instance::PatchRequest;
     use crate::function_instance::ResourceProviderSpecification;
     use crate::function_instance::SpawnFunctionRequest;
+    use crate::function_instance::StartResourceRequest;
     use crate::function_instance::StatePolicy;
     use crate::function_instance::StateSpecification;
     use crate::function_instance::UpdateNodeRequest;
@@ -782,6 +801,29 @@ mod tests {
         }];
         for msg in messages {
             match FunctonInstanceConverters::parse_spawn_function_request(&FunctonInstanceConverters::serialize_spawn_function_request(&msg)) {
+                Ok(val) => assert_eq!(msg, val),
+                Err(err) => panic!("{}", err),
+            }
+        }
+    }
+
+    #[test]
+    fn serialize_deserialize_start_resource_request() {
+        let messages = vec![
+            StartResourceRequest {
+                class_type: "provider-1".to_string(),
+                configurations: std::collections::HashMap::new(),
+            },
+            StartResourceRequest {
+                class_type: "provider-1".to_string(),
+                configurations: std::collections::HashMap::from([
+                    ("conf-1".to_string(), "value-1".to_string()),
+                    ("conf-2".to_string(), "value-2".to_string()),
+                ]),
+            },
+        ];
+        for msg in messages {
+            match FunctonInstanceConverters::parse_start_resource_request(&&FunctonInstanceConverters::serialize_start_resource_request(&msg)) {
                 Ok(val) => assert_eq!(msg, val),
                 Err(err) => panic!("{}", err),
             }
