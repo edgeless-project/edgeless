dcc9b4e72e3576377aae97de163476b673d446c8
Raphael Hetzel <hetzel@in.tum.de>
2024-01-03 16:24:38 +0000

    Runtime Split(WASM/Generic) and Cleanup

---
+++ edgeless_node/src/base_runtime/function_instance_runner.rs
@@ -0,0 +1,292 @@
+use futures::{FutureExt, SinkExt};
+use std::marker::PhantomData;
+
+use super::{FunctionInstance, FunctionInstanceError};
+
+/// This is the main interface for executing/managing a function instance.
+/// Owning client for a single function instance task.
+/// It is generic over the runtime technology (e.g. WASM).
+/// FunctionInstanceRunner (with it's FunctionInstanceTask) do most of the heavy lifting/lifetime management,
+/// while the technology specific implementations implement `FunctionInstance` interact and bind a virtualization technology.
+pub struct FunctionInstanceRunner<FunctionInstanceType: FunctionInstance> {
+    task_handle: Option<tokio::task::JoinHandle<()>>,
+    alias_mapping: super::alias_mapping::AliasMapping,
+    poison_pill_sender: Option<futures::channel::oneshot::Sender<()>>,
+    _instance: PhantomData<FunctionInstanceType>,
+}
+
+/// This is a runnable object (with all required state) actually executing a function.
+/// It is managed/owned by a FunctionInstanceRunner, which also runs it using a tokio task.
+struct FunctionInstanceTask<FunctionInstanceType: FunctionInstance> {
+    poison_pill_receiver: Option<futures::channel::oneshot::Receiver<()>>,
+    function_instance: Option<Box<FunctionInstanceType>>,
+    guest_api_host: Option<super::guest_api::GuestAPIHost>,
+    telemetry_handle: Box<dyn edgeless_telemetry::telemetry_events::TelemetryHandleAPI>,
+    code: Vec<u8>,
+    data_plane: edgeless_dataplane::handle::DataplaneHandle,
+    serialized_state: Option<String>,
+    init_payload: Option<String>,
+    runtime_api: futures::channel::mpsc::UnboundedSender<super::runtime::RuntimeRequest>,
+    instance_id: edgeless_api::function_instance::InstanceId,
+}
+
+impl<FunctionInstanceType: FunctionInstance> FunctionInstanceRunner<FunctionInstanceType> {
+    pub async fn new(
+        spawn_req: edgeless_api::function_instance::SpawnFunctionRequest,
+        data_plane: edgeless_dataplane::handle::DataplaneHandle,
+        runtime_api: futures::channel::mpsc::UnboundedSender<super::runtime::RuntimeRequest>,
+        state_handle: Box<dyn crate::state_management::StateHandleAPI>,
+        telemetry_handle: Box<dyn edgeless_telemetry::telemetry_events::TelemetryHandleAPI>,
+    ) -> Self {
+        let instance_id = spawn_req.instance_id.unwrap();
+        let mut telemetry_handle = telemetry_handle;
+        let mut state_handle = state_handle;
+
+        let alias_mapping = super::alias_mapping::AliasMapping::new();
+        // alias_mapping.update(spawn_req.output_mapping).await;
+        let (poison_pill_sender, poison_pill_receiver) = futures::channel::oneshot::channel::<()>();
+        let serialized_state = state_handle.get().await;
+
+        let guest_api_host = crate::base_runtime::guest_api::GuestAPIHost {
+            instance_id: instance_id.clone(),
+            data_plane: data_plane.clone(),
+            callback_table: alias_mapping.clone(),
+            state_handle: state_handle,
+            telemetry_handle: telemetry_handle.fork(std::collections::BTreeMap::new()),
+        };
+
+        let task = Box::new(
+            FunctionInstanceTask::<FunctionInstanceType>::new(
+                poison_pill_receiver,
+                telemetry_handle,
+                guest_api_host,
+                spawn_req.code.function_class_inlude_code.clone(),
+                data_plane,
+                serialized_state,
+                spawn_req.annotations.get("init-payload").map(|x| x.clone()),
+                runtime_api,
+                instance_id.clone(),
+            )
+            .await,
+        );
+
+        let task_handle = tokio::task::spawn(async move {
+            let mut task = task;
+            task.run().await;
+        });
+
+        Self {
+            task_handle: Some(task_handle),
+            alias_mapping: alias_mapping,
+            poison_pill_sender: Some(poison_pill_sender),
+            _instance: PhantomData {},
+        }
+    }
+
+    pub async fn stop(&mut self) {
+        if let Some(poison_pill_sender) = self.poison_pill_sender.take() {
+            poison_pill_sender.send(()).unwrap();
+        }
+        if let Some(handle) = self.task_handle.take() {
+            handle.await.unwrap();
+        }
+    }
+
+    pub async fn patch(&mut self, update_request: edgeless_api::common::PatchRequest) {
+        self.alias_mapping.update(update_request.output_mapping).await;
+    }
+}
+
+impl<FunctionInstanceType: FunctionInstance> FunctionInstanceTask<FunctionInstanceType> {
+    pub async fn new(
+        poison_pill_receiver: futures::channel::oneshot::Receiver<()>,
+        telemetry_handle: Box<dyn edgeless_telemetry::telemetry_events::TelemetryHandleAPI>,
+        guest_api_host: super::guest_api::GuestAPIHost,
+        code: Vec<u8>,
+        data_plane: edgeless_dataplane::handle::DataplaneHandle,
+        serialized_state: Option<String>,
+        init_param: Option<String>,
+        runtime_api: futures::channel::mpsc::UnboundedSender<super::runtime::RuntimeRequest>,
+        instance_id: edgeless_api::function_instance::InstanceId,
+    ) -> Self {
+        Self {
+            poison_pill_receiver: Some(poison_pill_receiver),
+            function_instance: None,
+            guest_api_host: Some(guest_api_host),
+            telemetry_handle: telemetry_handle,
+            code: code,
+            data_plane: data_plane,
+            serialized_state: serialized_state,
+            init_payload: init_param,
+            runtime_api: runtime_api,
+            instance_id: instance_id,
+        }
+    }
+
+    /// Function lifecycle; Runs until the poison pill is received or there is an error.
+    /// Always calls the exit handler (with the exit status)
+    pub async fn run(&mut self) {
+        let mut res = self.instantiate().await;
+        if res.is_ok() {
+            res = self.init().await;
+        }
+        if res.is_ok() {
+            res = self.processing_loop().await;
+        }
+        self.exit(res).await;
+    }
+
+    async fn instantiate(&mut self) -> Result<(), super::FunctionInstanceError> {
+        let start = tokio::time::Instant::now();
+
+        self.function_instance = Some(
+            FunctionInstanceType::instantiate(self.guest_api_host.take().ok_or(super::FunctionInstanceError::InternalError)?, &self.code).await?,
+        );
+
+        self.telemetry_handle.observe(
+            edgeless_telemetry::telemetry_events::TelemetryEvent::FunctionInstantiate(start.elapsed()),
+            std::collections::BTreeMap::new(),
+        );
+
+        Ok(())
+    }
+
+    async fn init(&mut self) -> Result<(), super::FunctionInstanceError> {
+        let start = tokio::time::Instant::now();
+
+        self.function_instance
+            .as_mut()
+            .ok_or(super::FunctionInstanceError::InternalError)?
+            .init(
+                self.init_payload.as_ref().map(|x| x.as_str()),
+                self.serialized_state.as_ref().map(|x| x.as_str()),
+            )
+            .await?;
+
+        self.telemetry_handle.observe(
+            edgeless_telemetry::telemetry_events::TelemetryEvent::FunctionInit(start.elapsed()),
+            std::collections::BTreeMap::new(),
+        );
+
+        Ok(())
+    }
+
+    async fn processing_loop(&mut self) -> Result<(), super::FunctionInstanceError> {
+        let mut poison_pill_recv = Box::pin(self.poison_pill_receiver.take().ok_or(super::FunctionInstanceError::InternalError)?).fuse();
+        loop {
+            futures::select! {
+                // Given each function instance is an independent task, the runtime needs to send a poison pill to cleanly stop it (processed here)
+                _ = poison_pill_recv => {
+                    return self.stop().await;
+                },
+                // Receive a normal event from the dataplane and invoke the function instance
+                edgeless_dataplane::core::DataplaneEvent{source_id, channel_id, message} =  Box::pin(self.data_plane.receive_next()).fuse() => {
+                    self.process_message(
+                        source_id,
+                        channel_id,
+                        message,
+                    ).await?;
+                }
+            }
+        }
+    }
+
+    async fn process_message(
+        &mut self,
+        source_id: edgeless_api::function_instance::InstanceId,
+        channel_id: u64,
+        message: edgeless_dataplane::core::Message,
+    ) -> Result<(), super::FunctionInstanceError> {
+        match message {
+            edgeless_dataplane::core::Message::Cast(payload) => self.process_cast_message(source_id.clone(), payload).await,
+            edgeless_dataplane::core::Message::Call(payload) => self.process_call_message(source_id, payload, channel_id).await,
+            _ => {
+                log::debug!("Unprocessed Message");
+                Ok(())
+            }
+        }
+    }
+
+    async fn process_cast_message(
+        &mut self,
+        source_id: edgeless_api::function_instance::InstanceId,
+        payload: String,
+    ) -> Result<(), super::FunctionInstanceError> {
+        let start = tokio::time::Instant::now();
+
+        self.function_instance
+            .as_mut()
+            .ok_or(super::FunctionInstanceError::InternalError)?
+            .cast(&source_id, &payload)
+            .await?;
+
+        self.telemetry_handle.observe(
+            edgeless_telemetry::telemetry_events::TelemetryEvent::FunctionInvocationCompleted(start.elapsed()),
+            std::collections::BTreeMap::from([("EVENT_TYPE".to_string(), "CAST".to_string())]),
+        );
+        Ok(())
+    }
+
+    async fn process_call_message(
+        &mut self,
+        source_id: edgeless_api::function_instance::InstanceId,
+        payload: String,
+        channel_id: u64,
+    ) -> Result<(), super::FunctionInstanceError> {
+        let start = tokio::time::Instant::now();
+
+        let res = self
+            .function_instance
+            .as_mut()
+            .ok_or(super::FunctionInstanceError::InternalError)?
+            .call(&source_id, &payload)
+            .await?;
+
+        self.telemetry_handle.observe(
+            edgeless_telemetry::telemetry_events::TelemetryEvent::FunctionInvocationCompleted(start.elapsed()),
+            std::collections::BTreeMap::from([("EVENT_TYPE".to_string(), "CALL".to_string())]),
+        );
+
+        let mut wh = self.data_plane.clone();
+        wh.reply(source_id, channel_id, res).await;
+        Ok(())
+    }
+
+    async fn stop(&mut self) -> Result<(), super::FunctionInstanceError> {
+        let start = tokio::time::Instant::now();
+
+        self.function_instance
+            .as_mut()
+            .ok_or(super::FunctionInstanceError::InternalError)?
+            .stop()
+            .await?;
+
+        self.telemetry_handle.observe(
+            edgeless_telemetry::telemetry_events::TelemetryEvent::FunctionStop(start.elapsed()),
+            std::collections::BTreeMap::new(),
+        );
+
+        Ok(())
+    }
+
+    async fn exit(&mut self, exit_status: Result<(), super::FunctionInstanceError>) {
+        self.runtime_api
+            .send(super::runtime::RuntimeRequest::FunctionExit(
+                self.instance_id.clone(),
+                exit_status.clone(),
+            ))
+            .await
+            .unwrap_or_else(|_| log::error!("FunctionInstance outlived runner."));
+
+        self.telemetry_handle.observe(
+            edgeless_telemetry::telemetry_events::TelemetryEvent::FunctionExit(match exit_status {
+                Ok(_) => edgeless_telemetry::telemetry_events::FunctionExitStatus::Ok,
+                Err(exit_err) => match exit_err {
+                    FunctionInstanceError::BadCode => edgeless_telemetry::telemetry_events::FunctionExitStatus::CodeError,
+                    _ => edgeless_telemetry::telemetry_events::FunctionExitStatus::InternalError,
+                },
+            }),
+            std::collections::BTreeMap::new(),
+        );
+    }
+}
---
+++ edgeless_node/src/base_runtime/runtime.rs
@@ -0,0 +1,154 @@
+/// Generic function runtime hosting a set of runners of one type (e.g. WASM/Docker)
+/// Split into the active component `RuntimeTask` and the cloneable `RuntimeClient` allowing to interact with the runtime.
+use futures::{SinkExt, StreamExt};
+
+#[derive(Clone)]
+pub struct RuntimeClient {
+    sender: futures::channel::mpsc::UnboundedSender<RuntimeRequest>,
+}
+
+pub struct RuntimeTask<FunctionInstanceType: super::FunctionInstance> {
+    receiver: futures::channel::mpsc::UnboundedReceiver<RuntimeRequest>,
+    data_plane_provider: edgeless_dataplane::handle::DataplaneProvider,
+    state_manager: Box<dyn crate::state_management::StateManagerAPI>,
+    telemetry_handle: Box<dyn edgeless_telemetry::telemetry_events::TelemetryHandleAPI>,
+    slf_channel: futures::channel::mpsc::UnboundedSender<RuntimeRequest>,
+    functions: std::collections::HashMap<uuid::Uuid, super::function_instance_runner::FunctionInstanceRunner<FunctionInstanceType>>,
+}
+
+pub enum RuntimeRequest {
+    Start(edgeless_api::function_instance::SpawnFunctionRequest),
+    Stop(edgeless_api::function_instance::InstanceId),
+    Patch(edgeless_api::common::PatchRequest),
+    FunctionExit(edgeless_api::function_instance::InstanceId, Result<(), super::FunctionInstanceError>),
+}
+
+/// Entrypoint for all runtimes based on the base_runtime.
+pub fn create<FunctionInstanceType: super::FunctionInstance>(
+    data_plane_provider: edgeless_dataplane::handle::DataplaneProvider,
+    state_manager: Box<dyn crate::state_management::StateManagerAPI>,
+    telemetry_handle: Box<dyn edgeless_telemetry::telemetry_events::TelemetryHandleAPI>,
+) -> (RuntimeClient, RuntimeTask<FunctionInstanceType>) {
+    let (sender, receiver) = futures::channel::mpsc::unbounded();
+    let task = RuntimeTask::new(receiver, data_plane_provider, state_manager, telemetry_handle, sender.clone());
+
+    let client = RuntimeClient::new(sender);
+
+    (client, task)
+}
+
+impl<FunctionInstanceType: super::FunctionInstance> RuntimeTask<FunctionInstanceType> {
+    fn new(
+        receiver: futures::channel::mpsc::UnboundedReceiver<RuntimeRequest>,
+        data_plane_provider: edgeless_dataplane::handle::DataplaneProvider,
+        state_manager: Box<dyn crate::state_management::StateManagerAPI>,
+        telemetry_handle: Box<dyn edgeless_telemetry::telemetry_events::TelemetryHandleAPI>,
+        slf_channel: futures::channel::mpsc::UnboundedSender<RuntimeRequest>,
+    ) -> Self {
+        Self {
+            receiver: receiver,
+            data_plane_provider: data_plane_provider,
+            state_manager: state_manager,
+            telemetry_handle,
+            slf_channel: slf_channel,
+            functions: std::collections::HashMap::new(),
+        }
+    }
+
+    pub async fn run(&mut self) {
+        log::info!("Starting Edgeless Runner");
+        while let Some(req) = self.receiver.next().await {
+            match req {
+                RuntimeRequest::Start(spawn_request) => {
+                    self.start_function(spawn_request).await;
+                }
+                RuntimeRequest::Stop(instance_id) => {
+                    self.stop_function(instance_id).await;
+                }
+                RuntimeRequest::Patch(update_request) => {
+                    self.patch_function_links(update_request).await;
+                }
+                RuntimeRequest::FunctionExit(id, status) => {
+                    self.function_exit(id, status).await;
+                }
+            }
+        }
+    }
+
+    async fn start_function(&mut self, spawn_request: edgeless_api::function_instance::SpawnFunctionRequest) {
+        log::info!("Start Function {:?}", spawn_request.instance_id);
+        let instance_id = match spawn_request.instance_id.clone() {
+            Some(id) => id,
+            None => {
+                return;
+            }
+        };
+        let cloned_req = spawn_request.clone();
+        let data_plane = self.data_plane_provider.get_handle_for(instance_id.clone()).await;
+        let instance = super::function_instance_runner::FunctionInstanceRunner::new(
+            cloned_req,
+            data_plane,
+            self.slf_channel.clone(),
+            self.state_manager
+                .get_handle(spawn_request.state_specification.state_policy, spawn_request.state_specification.state_id)
+                .await,
+            self.telemetry_handle.fork(std::collections::BTreeMap::from([(
+                "FUNCTION_ID".to_string(),
+                instance_id.function_id.to_string(),
+            )])),
+        )
+        .await;
+        self.functions.insert(instance_id.function_id.clone(), instance);
+    }
+
+    async fn stop_function(&mut self, instance_id: edgeless_api::function_instance::InstanceId) {
+        log::info!("Stop Function {:?}", instance_id);
+        if let Some(instance) = self.functions.get_mut(&instance_id.function_id) {
+            instance.stop().await;
+        }
+    }
+
+    async fn patch_function_links(&mut self, update_request: edgeless_api::common::PatchRequest) {
+        log::info!("Patch Function {:?}", update_request.function_id);
+        if let Some(instance) = self.functions.get_mut(&update_request.function_id) {
+            instance.patch(update_request).await;
+        }
+    }
+
+    async fn function_exit(&mut self, instance_id: edgeless_api::function_instance::InstanceId, status: Result<(), super::FunctionInstanceError>) {
+        log::info!("Function Exit Event: {:?} {:?}", instance_id, status);
+        self.functions.remove(&instance_id.function_id);
+    }
+}
+
+impl RuntimeClient {
+    pub fn new(runtime_request_sender: futures::channel::mpsc::UnboundedSender<RuntimeRequest>) -> Self {
+        RuntimeClient {
+            sender: runtime_request_sender,
+        }
+    }
+}
+
+#[async_trait::async_trait]
+impl super::RuntimeAPI for RuntimeClient {
+    async fn start(&mut self, request: edgeless_api::function_instance::SpawnFunctionRequest) -> anyhow::Result<()> {
+        match self.sender.send(RuntimeRequest::Start(request)).await {
+            Ok(_) => Ok(()),
+            Err(_) => Err(anyhow::anyhow!("Runner Channel Error")),
+        }
+    }
+
+    async fn stop(&mut self, instance_id: edgeless_api::function_instance::InstanceId) -> anyhow::Result<()> {
+        match self.sender.send(RuntimeRequest::Stop(instance_id)).await {
+            Ok(_) => Ok(()),
+            Err(_) => Err(anyhow::anyhow!("Runner Channel Error")),
+        }
+    }
+
+    async fn patch(&mut self, update: edgeless_api::common::PatchRequest) -> anyhow::Result<()> {
+        match self.sender.send(RuntimeRequest::Patch(update)).await {
+            Ok(_) => Ok(()),
+            Err(_) => Err(anyhow::anyhow!("Runner Channel Error")),
+        }
+    }
+}
