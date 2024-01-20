8e48f144afba494e9236e9b62759dc03d8c0a7cf
Claudio Cicconetti <c.cicconetti@iit.cnr.it>
2024-01-20 14:38:20 +0100

    benchmark: remove use of std::time::now() from matrix_mul function

--- edgeless_benchmark/functions/matrix_mul/src/lib.rs
+++ edgeless_benchmark/functions/matrix_mul/src/lib.rs
@@ -35,17 +35,25 @@ fn make_new_matrix(lcg: &mut Lcg, size: usize) -> Vec<f32> {
 }
 
 struct Conf {
-    inter_arrival: std::time::Duration,
+    // Interarrival time before the next event, in ms. Only if is_first == true.
+    inter_arrival: u64,
+    // True: this is the first function in the workflow.
     is_first: bool,
+    // True: this is the last function in the workflow.
     is_last: bool,
+    // Name of the workflow (for stats only).
     wf_name: String,
+    // Name of the function (for stats only).
     fun_name: String,
+    // Which outputs are enabled.
     outputs: std::collections::HashSet<String>,
 }
 struct State {
+    // ID of the next transaction. Only used if is_first == true.
     next_id: usize,
-    next_arrival: std::time::Instant,
+    // Pseudo-random number generator.
     lcg: Lcg,
+    // Matrix of values to consume CPU.
     matrix: Vec<f32>,
 }
 
@@ -72,15 +80,24 @@ fn parse_init(payload: &str) -> std::collections::HashMap<&str, &str> {
 
 impl Edgefunction for MatrixMulFunction {
     fn handle_cast(_src: InstanceId, encoded_message: String) {
-        let ts_start = std::time::Instant::now();
-
         let conf = CONF.get().unwrap();
         log::info!("MatrixMul casted, wf {}, fun {}, MSG: {}", conf.wf_name, conf.fun_name, encoded_message);
-
         let mut state = STATE.get().unwrap().lock().unwrap();
-        let n = state.matrix.len();
+
+        // Schedule the next transaction.
+        let id = match conf.is_first {
+            true => {
+                delayed_cast(conf.inter_arrival, "self", "");
+                cast("metric", format!("workflow:start:{}:{}", conf.wf_name, state.next_id).as_str());
+                state.next_id += 1;
+                state.next_id - 1
+            }
+            false => encoded_message.parse::<usize>().unwrap_or(0),
+        };
+        cast("metric", format!("function:start:{}:{}:{}", conf.wf_name, conf.fun_name, id).as_str());
 
         // Fill a new matrix with random numbers.
+        let n = state.matrix.len();
         let random_matrix = make_new_matrix(&mut state.lcg, n);
 
         // Multiply previous matrix by the random one.
@@ -97,45 +114,16 @@ impl Edgefunction for MatrixMulFunction {
             }
         }
         state.matrix = output_matrix;
-        let ts_end = std::time::Instant::now();
-        let id = match conf.is_first {
-            true => state.next_id,
-            false => encoded_message.parse::<usize>().unwrap_or(0),
-        };
 
-        // Save metrics.
-        if conf.is_first {
-            cast(
-                "metrics",
-                format!("{}:{}:start:{}", conf.wf_name, id, ts_start.elapsed().as_micros()).as_str(),
-            );
-        }
+        // Save metrics at the end of the execution.
         if conf.is_last {
-            cast(
-                "metrics",
-                format!("{}:{}:end:{}", conf.wf_name, id, ts_end.elapsed().as_micros()).as_str(),
-            );
+            cast("metric", format!("workflow:end:{}:{}", conf.wf_name, id).as_str());
         }
-        cast(
-            "metrics",
-            format!("{}:{}:{}:{}", conf.wf_name, conf.fun_name, id, (ts_end - ts_start).as_micros()).as_str(),
-        );
+        cast("metric", format!("function:end:{}:{}:{}", conf.wf_name, conf.fun_name, id).as_str());
 
         // Call outputs
         for output in &conf.outputs {
             cast(output, format!("{}", id).as_str());
-        }
-
-        // Schedule next event.
-        if conf.is_first {
-            state.next_id += 1;
-            if state.next_arrival > ts_end {
-                let time_until_next_arrival = state.next_arrival - ts_end;
-                delayed_cast(time_until_next_arrival.as_millis() as u64, "self", "");
-            } else {
-                cast("self", "");
-            }
-            state.next_arrival += conf.inter_arrival;
         }
     }
 
@@ -145,7 +133,7 @@ impl Edgefunction for MatrixMulFunction {
     }
 
     // example of payload:
-    // seed=42,inter_arrival=5.0,is_first=true,is_last=false,wf_name=my_workflow,fun_name=my_function,matrix_size=1000,outputs=0:2:19
+    // seed=42,inter_arrival=2000,is_first=true,is_last=false,wf_name=my_workflow,fun_name=my_function,matrix_size=1000,outputs=0:2:19
     fn handle_init(payload: String, _serialized_state: Option<String>) {
         edgeless_function::init_logger();
         log::info!("MatrixMul initialized, payload: {}", payload);
@@ -153,7 +141,7 @@ impl Edgefunction for MatrixMulFunction {
 
         let seed = arguments.get("seed").unwrap_or(&"0").parse::<u32>().unwrap_or(0);
 
-        let inter_arrival = std::time::Duration::from_secs_f32(arguments.get("inter_arrival").unwrap_or(&"1.0").parse::<f32>().unwrap_or(1.0));
+        let inter_arrival = arguments.get("inter_arrival").unwrap_or(&"1000").parse::<u64>().unwrap_or(1000);
         let is_first = arguments.get("is_first").unwrap_or(&"false").to_lowercase() == "true";
         let is_last = arguments.get("is_last").unwrap_or(&"false").to_lowercase() == "true";
         let wf_name = arguments.get("wf_name").unwrap_or(&"no-wf-name").to_string();
@@ -184,18 +172,7 @@ impl Edgefunction for MatrixMulFunction {
         let mut lcg = Lcg::new(seed);
         let matrix = make_new_matrix(&mut lcg, matrix_size);
 
-        log::info!("XXX");
-        let t = std::time::Instant::now();
-        log::info!("XXX {:?}", t);
-        let next_arrival = t + inter_arrival;
-        log::info!("XXX {:?}", t);
-
-        let _ = STATE.set(std::sync::Mutex::new(State {
-            next_id: 0,
-            next_arrival,
-            lcg,
-            matrix,
-        }));
+        let _ = STATE.set(std::sync::Mutex::new(State { next_id: 0, lcg, matrix }));
 
         if is_first {
             cast("self", "");
