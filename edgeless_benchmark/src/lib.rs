8eaeff15918afaa0cda7ffe25ad3898445d9bed2
Claudio Cicconetti <c.cicconetti@iit.cnr.it>
2024-02-06 16:21:38 +0100

    Add skeleton of a class to dump the content of data in Redis to a file

--- edgeless_benchmark/src/lib.rs
+++ edgeless_benchmark/src/lib.rs
@@ -1,8 +1,8 @@
 // SPDX-FileCopyrightText: © 2023 Claudio Cicconetti <c.cicconetti@iit.cnr.it>
 // SPDX-License-Identifier: MIT
 
+pub mod redis_dumper;
 pub mod resources;
-
 pub struct DummyFunctionInstance {}
 
 #[async_trait::async_trait]
