9a678d5b99bbf7dc96d12598d70f20e02db03ff3
Raphael Hetzel <hetzel@in.tum.de>
2024-02-27 09:50:05 +0000

    Update Wasmtime Runner to the non_component_model interface.

--- edgeless_node/src/wasm_runner/mod.rs
+++ edgeless_node/src/wasm_runner/mod.rs
@@ -1,9 +1,5 @@
 // SPDX-FileCopyrightText: © 2023 Technical University of Munich, Chair of Connected Mobility
 // SPDX-License-Identifier: MIT
-/// Generated host-side of the WASM Component Model.
-pub mod wit_binding {
-    wasmtime::component::bindgen!({path: "../edgeless_function/wit/edgefunction.wit", async: true});
-}
 
 /// Main Implementation of a WASM component function instance.
 /// Note that this module only contains the WASM specifics, the generic parts are implemented in the base_runtime.
@@ -11,6 +7,8 @@ pub mod function_instance;
 
 /// Bridge between the guest_api_host and the interface defined in the wit binding
 pub mod guest_api_binding;
+
+mod helpers;
 
 #[cfg(test)]
 mod test;
