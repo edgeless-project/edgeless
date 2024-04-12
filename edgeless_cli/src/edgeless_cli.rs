31d64e2a17313d34d5642e23b8e354fbe07dbc20
Claudio Cicconetti <c.cicconetti@iit.cnr.it>
2024-04-12 10:40:00 +0000

    Add FunctionInstance skeleton for container run-time
    Update edgeless_cli so that it does not load the binary from filesystem unless the run-time is RUST_WASM

--- edgeless_cli/src/edgeless_cli.rs
+++ edgeless_cli/src/edgeless_cli.rs
@@ -94,17 +94,25 @@ async fn main() -> anyhow::Result<()> {
                                     .functions
                                     .into_iter()
                                     .map(|func_spec| {
-                                        let p = std::path::Path::new(&spec_file)
-                                            .parent()
-                                            .unwrap()
-                                            .join(func_spec.class_specification.code.unwrap());
+                                        let function_class_code = match func_spec.class_specification.function_type.as_str() {
+                                            "RUST_WASM" => std::fs::read(
+                                                std::path::Path::new(&spec_file)
+                                                    .parent()
+                                                    .unwrap()
+                                                    .join(func_spec.class_specification.code.unwrap()),
+                                            )
+                                            .unwrap(),
+                                            "CONTAINER" => func_spec.class_specification.code.unwrap().as_bytes().to_vec(),
+                                            _ => panic!("unknown function class type: {}", func_spec.class_specification.function_type),
+                                        };
+
                                         edgeless_api::workflow_instance::WorkflowFunction {
                                             name: func_spec.name,
                                             function_class_specification: edgeless_api::function_instance::FunctionClassSpecification {
                                                 function_class_id: func_spec.class_specification.id,
                                                 function_class_type: func_spec.class_specification.function_type,
                                                 function_class_version: func_spec.class_specification.version,
-                                                function_class_code: std::fs::read(p).unwrap(),
+                                                function_class_code,
                                                 function_class_outputs: func_spec.class_specification.outputs,
                                             },
                                             output_mapping: func_spec.output_mapping,
