ad6d8be50682fc20ef3815c6098e9e6d136f0556
Claudio Cicconetti <c.cicconetti@iit.cnr.it>
2024-04-17 13:59:05 +0000

    Code maquillage

--- edgeless_node/src/bin/edgeless_container_function_d.rs
+++ edgeless_node/src/bin/edgeless_container_function_d.rs
@@ -2,7 +2,6 @@
 // SPDX-License-Identifier: MIT
 
 use clap::Parser;
-use edgeless_node::container_runner;
 
 #[derive(Debug, clap::Parser)]
 #[command(long_about = None)]
@@ -12,7 +11,7 @@ struct Args {
 }
 
 async fn edgeless_container_function_main(endpoint: String) {
-    let (mut container_function, container_function_task) = container_runner::container_function::ContainerFunction::new();
+    let (mut container_function, container_function_task) = edgeless_node::container_runner::container_function::ContainerFunction::new();
     let server_task = edgeless_api::grpc_impl::container_function::GuestAPIFunctionServer::run(container_function.get_api_client(), endpoint);
     futures::join!(container_function_task, server_task);
 }
