e495f3adb38741c4c18b5a69464fa818b931c035
Claudio Cicconetti <c.cicconetti@iit.cnr.it>
2024-04-17 14:06:39 +0000

    Code reorganization: move edgeless_container_function to own crate

--- edgeless_node/src/container_runner/mod.rs
+++ edgeless_node/src/container_runner/mod.rs
@@ -1,8 +1,6 @@
 // SPDX-FileCopyrightText: © 2024 Claudio Cicconetti <c.cicconetti@iit.cnr.it>
 // SPDX-License-Identifier: MIT
 
-pub mod container_function;
-
 pub mod container_runtime;
 
 pub mod function_instance;
