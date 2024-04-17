269e755628868ebfd1b0aba2c057491b118b1b07
pantoni <p.antoniou@aegisresearch.eu>
2024-04-17 08:32:30 +0000

    elasticsearch API integration 1st version

--- edgeless_telemetry/src/lib.rs
+++ edgeless_telemetry/src/lib.rs
@@ -1,4 +1,6 @@
 // SPDX-FileCopyrightText: © 2023 Technical University of Munich, Chair of Connected Mobility
 // SPDX-License-Identifier: MIT
+pub mod elasticsearch_api;
+pub mod elasticsearch_resources;
 pub mod prometheus_target;
 pub mod telemetry_events;
