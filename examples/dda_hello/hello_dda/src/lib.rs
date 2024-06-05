8b430fa4cf17160b72683e3d374ae9b6fe21fdad
Claudio Cicconetti <c.cicconetti@iit.cnr.it>
2024-06-05 11:19:05 +0000

    Fix dda_hello
    The library was using old APIs with messages as strings

--- examples/dda_hello/hello_dda/src/lib.rs
+++ examples/dda_hello/hello_dda/src/lib.rs
@@ -1,25 +1,27 @@
 // SPDX-FileCopyrightText: © 2024 Siemens AG
 // SPDX-License-Identifier: MIT
 
-use edgeless_function::api::*;
+use edgeless_function::*;
 
 struct HelloDDA;
 
-impl Edgefunction for HelloDDA {
-    fn handle_cast(_src: InstanceId, message: String) {
-        cast("file_log_output", "HelloDDA");
-        cast("dda_output", "Hello DDA");
+impl EdgeFunction for HelloDDA {
+    fn handle_cast(_src: InstanceId, encoded_message: &[u8]) {
+        cast("file_log_output", b"HelloDDA");
+        cast("dda_output", b"Hello DDA");
 
-        delayed_cast(1000, "self", &message);
+        delayed_cast(1000, "self", &encoded_message);
     }
 
-    fn handle_call(_src: InstanceId, _message: String) -> CallRet {
-        CallRet::Noreply
+    fn handle_call(_src: InstanceId, _encoded_message: &[u8]) -> CallRet {
+        CallRet::NoReply
     }
 
-    fn handle_init(init_message: String, _serialized_state: Option<String>) {
+    fn handle_init(init_message: Option<&[u8]>, _serialized_state: Option<&[u8]>) {
         edgeless_function::init_logger();
-        cast("self", &init_message);
+        if let Some(init_message) = init_message {
+            cast("self", &init_message);
+        }
     }
 
     fn handle_stop() {
