fab589fd61a0e96fec1886995875cfbc30c6c5d1
Claudio Cicconetti <c.cicconetti@iit.cnr.it>
2024-06-10 08:57:03 +0000

    WIP: implement Redis serialization of the orchestration data structures

--- edgeless_orc/src/proxy_redis.rs
+++ edgeless_orc/src/proxy_redis.rs
@@ -5,9 +5,24 @@ use redis::Commands;
 
 /// An orchestrator proxy that uses a Redis in-memory database to mirror
 /// internal data structures and read orchestration intents.
+///
+/// The whole database is flushed upon initialiation.
+///
+/// The following keys are written:
+/// - node:capabilities::UUID, where UUID is the node identifier
+/// - node:health::UUID, where UUID is the node identifier
+/// - provider::ID, where ID is the resource provider identifier
+/// - instance::UUID, where UUID is the logical function/resource identifier
+/// - dependency::UUID, where UUID is the logical function/resource identifier
+///
+/// All the values are JSON structures.
+///
 pub struct ProxyRedis {
     connection: redis::Connection,
     node_uuids: std::collections::HashSet<uuid::Uuid>,
+    resource_provider_ids: std::collections::HashSet<String>,
+    active_instance_uuids: std::collections::HashSet<uuid::Uuid>,
+    dependency_uuids: std::collections::HashSet<uuid::Uuid>,
 }
 
 impl ProxyRedis {
@@ -23,6 +38,9 @@ impl ProxyRedis {
         Ok(Self {
             connection,
             node_uuids: std::collections::HashSet::new(),
+            resource_provider_ids: std::collections::HashSet::new(),
+            active_instance_uuids: std::collections::HashSet::new(),
+            dependency_uuids: std::collections::HashSet::new(),
         })
     }
 }
@@ -30,39 +48,86 @@ impl ProxyRedis {
 impl super::proxy::Proxy for ProxyRedis {
     fn update_nodes(&mut self, nodes: &std::collections::HashMap<uuid::Uuid, super::orchestrator::ClientDesc>) {
         // serialize the nodes' capabilities and health status to Redis
-        nodes
-            .iter()
-            .map(|(uuid, client_desc)| {
-                self.connection
-                    .hset::<&str, &str, &str, usize>(
-                        format!("node:{}", uuid).as_str(),
-                        "capabilities",
-                        serde_json::to_string(&client_desc.capabilities).unwrap_or_default().as_str(),
-                    )
-                    .is_ok()
-                    && self
-                        .connection
-                        .hset::<&str, &str, &str, usize>(
-                            format!("node:{}", uuid).as_str(),
-                            "health_status",
-                            serde_json::to_string(&client_desc.health_status).unwrap_or_default().as_str(),
-                        )
-                        .is_ok()
-            })
-            .all(|x| x == true);
+        for (uuid, client_desc) in nodes {
+            redis::pipe()
+                .set::<&str, &str>(
+                    format!("node:capabilities:{}", uuid).as_str(),
+                    serde_json::to_string(&client_desc.capabilities).unwrap_or_default().as_str(),
+                )
+                .set::<&str, &str>(
+                    format!("node:health:{}", uuid).as_str(),
+                    serde_json::to_string(&client_desc.health_status).unwrap_or_default().as_str(),
+                )
+                .execute(&mut self.connection);
+        }
 
         // remove nodes that are not anymore in the orchestration domain
-        let new_node_uuids = nodes.keys().cloned().collect::<std::collections::HashSet<uuid::Uuid>>();
-        self.node_uuids
-            .difference(&new_node_uuids)
-            .map(|uuid| {
-                self.connection
-                    .hdel::<&str, Vec<&str>, usize>(format!("node:{}", uuid).as_str(), vec!["capabilities", "health_status"])
-                    .is_ok()
-            })
-            .all(|x| x == true);
+        let new_active_instance_uuids = nodes.keys().cloned().collect::<std::collections::HashSet<uuid::Uuid>>();
+        self.active_instance_uuids.difference(&new_active_instance_uuids).for_each(|uuid| {
+            redis::pipe()
+                .del(format!("node:capabilities:{}", uuid).as_str())
+                .del(format!("node:health:{}", uuid).as_str())
+                .execute(&mut self.connection);
+        });
 
         // update the list of node UUIDs
+        self.active_instance_uuids = new_active_instance_uuids;
+    }
+
+    fn update_resource_providers(&mut self, resource_providers: &std::collections::HashMap<String, super::orchestrator::ResourceProvider>) {
+        // serialize the resource providers
+        for (provider_id, resource_provider) in resource_providers {
+            let _ = self.connection.set::<&str, &str, usize>(
+                format!("provider:{}", provider_id).as_str(),
+                serde_json::to_string(&resource_provider).unwrap_or_default().as_str(),
+            );
+        }
+
+        // remove resource providers that are not anymore present
+        let new_resource_provider_ids = resource_providers.keys().cloned().collect::<std::collections::HashSet<String>>();
+        self.resource_provider_ids.difference(&new_resource_provider_ids).for_each(|provider_id| {
+            let _ = self.connection.del::<&str, usize>(format!("provider:{}", provider_id).as_str());
+        });
+
+        // update the list of the resource provider identifiers
+        self.resource_provider_ids = new_resource_provider_ids;
+    }
+
+    fn update_active_instances(&mut self, active_instances: &std::collections::HashMap<uuid::Uuid, super::orchestrator::ActiveInstance>) {
+        // serialize the active instances
+        for (ext_fid, active_instance) in active_instances {
+            let _ = self.connection.set::<&str, &str, usize>(
+                format!("instance:{}", ext_fid).as_str(),
+                serde_json::to_string(&active_instance).unwrap_or_default().as_str(),
+            );
+        }
+
+        // remove instances that are not active anymore
+        let new_node_uuids = active_instances.keys().cloned().collect::<std::collections::HashSet<uuid::Uuid>>();
+        self.node_uuids.difference(&new_node_uuids).for_each(|ext_fid| {
+            let _ = self.connection.del::<&str, usize>(format!("instance:{}", ext_fid).as_str());
+        });
+
+        // update the list of active instance ext fids
         self.node_uuids = new_node_uuids;
+    }
+
+    fn update_dependency_graph(&mut self, dependency_graph: &std::collections::HashMap<uuid::Uuid, std::collections::HashMap<String, uuid::Uuid>>) {
+        // serialize the dependency graph
+        for (ext_fid, dependencies) in dependency_graph {
+            let _ = self.connection.set::<&str, &str, usize>(
+                format!("dependency:{}", ext_fid).as_str(),
+                serde_json::to_string(&dependencies).unwrap_or_default().as_str(),
+            );
+        }
+
+        // remove dependencies that do not exist anymore
+        let new_dependency_uuids = dependency_graph.keys().cloned().collect::<std::collections::HashSet<uuid::Uuid>>();
+        self.dependency_uuids.difference(&new_dependency_uuids).for_each(|ext_fid| {
+            let _ = self.connection.del::<&str, usize>(format!("dependency:{}", ext_fid).as_str());
+        });
+
+        // update the list of active instance ext fids
+        self.dependency_uuids = new_dependency_uuids;
     }
 }
