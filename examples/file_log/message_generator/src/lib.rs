702b14d30ef713b16ea2fc9a78495142dd5c4f56
Claudio Cicconetti <c.cicconetti@iit.cnr.it>
2024-06-13 10:33:04 +0000

    Enhanced a bit the message_generator in examples/file_log
    Add configuration of the period and message in the init-payload annotation.
    Print a counter initialized from 0 at every message.

--- examples/file_log/message_generator/src/lib.rs
+++ examples/file_log/message_generator/src/lib.rs
@@ -4,30 +4,57 @@ use edgeless_function::*;
 
 struct MessageGenerator;
 
+struct InitState {
+    period: u64,
+}
+
+struct State {
+    counter: u64,
+}
+
+static INIT_STATE: std::sync::OnceLock<InitState> = std::sync::OnceLock::new();
+static STATE: std::sync::OnceLock<std::sync::Mutex<State>> = std::sync::OnceLock::new();
+
 impl EdgeFunction for MessageGenerator {
     fn handle_cast(src: InstanceId, message: &[u8]) {
+        let init_state = INIT_STATE.get().unwrap();
+        let mut state = STATE.get().unwrap().lock().unwrap();
+
         cast(
             "output",
             format!(
-                "{} from {:?}:{:?}",
-                &core::str::from_utf8(message).unwrap(),
+                "from node_id {} function_id {} [#{}]: {}",
                 uuid::Uuid::from_bytes(src.node_id).to_string(),
-                uuid::Uuid::from_bytes(src.component_id).to_string()
+                uuid::Uuid::from_bytes(src.component_id).to_string(),
+                state.counter,
+                &core::str::from_utf8(message).unwrap()
             )
             .as_bytes(),
         );
-        delayed_cast(1000, "self", &message);
+        state.counter += 1;
+        delayed_cast(init_state.period, "self", &message);
     }
 
     fn handle_call(_src: InstanceId, _message: &[u8]) -> CallRet {
         CallRet::NoReply
     }
 
-    fn handle_init(init_message: Option<&[u8]>, _serialized_state: Option<&[u8]>) {
+    fn handle_init(payload: Option<&[u8]>, _serialized_state: Option<&[u8]>) {
         edgeless_function::init_logger();
-        if let Some(init_message) = init_message {
-            cast("self", &init_message);
-        }
+        let arguments = if let Some(payload) = payload {
+            let str_payload = core::str::from_utf8(payload).unwrap();
+            edgeless_function::parse_init_payload(str_payload)
+        } else {
+            std::collections::HashMap::new()
+        };
+
+        let period = arguments.get("period").unwrap_or(&"1000").parse::<u64>().unwrap_or(1000);
+        let message = arguments.get("message").unwrap_or(&"hello world");
+        let _ = INIT_STATE.set(InitState { period });
+
+        let _ = STATE.set(std::sync::Mutex::new(State { counter: 0 }));
+
+        cast("self", message.as_bytes());
     }
 
     fn handle_stop() {
