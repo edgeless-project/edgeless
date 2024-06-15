541178c505b31acac2ce984335f126e9975202a2
Claudio Cicconetti <c.cicconetti@iit.cnr.it>
2024-06-15 07:47:48 +0000

    Update deployment scripts

--- deployment/orchestrator/start.sh
+++ deployment/orchestrator/start.sh
@@ -1,17 +1,32 @@
 #!/bin/bash
 
-ports="7011 7121 7102"
+orc_addr_port=(${ORCHESTRATOR_ENDPOINT//:/ })
+agent_addr_port=(${AGENT_ENDPOINT//:/ })
+invocation_addr_port=(${INVOCATION_ENDPOINT//:/ })
 
-rm -f controller.toml
-/usr/src/myapp/edgeless/target/release/edgeless_orc_d -t orchestrator.toml
-for port in $ports ; do
-    sed -i -e "s/127.0.0.1:$port/0.0.0.0:$port/" orchestrator.toml
-done
-sed -i \
-    -e 's/proxy_type = "None"/proxy_type = "Redis"/' \
-    -e 's/collector_type = "None"/collector_type = "Redis"/' \
-    -e "s/redis_url = \"\"/redis_url = \"redis:\/\/$REDIS_ENDPOINT\"/" \
-    orchestrator.toml
+cat > orchestrator.toml << EOF
+[general]
+domain_id = "domain-1"
+orchestrator_url = "http://0.0.0.0:${orc_addr_port[1]}"
+orchestrator_url_announced = "http://$ORCHESTRATOR_ENDPOINT"
+agent_url = "http://0.0.0.0:${agent_addr_port[1]}"
+agent_url_announced = "http://$AGENT_ENDPOINT"
+invocation_url = "http://0.0.0.0:${invocation_addr_port[1]}"
+invocation_url_announced = "http://$INVOCATION_ENDPOINT"
+
+[baseline]
+orchestration_strategy = "Random"
+keep_alive_interval_secs = 2
+
+[proxy]
+proxy_type = "Redis"
+redis_url = "redis://$REDIS_ENDPOINT"
+
+[collector]
+collector_type = "Redis"
+redis_url = "redis://$REDIS_ENDPOINT"
+EOF
+
 if [ "$SHOWCONF" != "" ] ; then
     cat orchestrator.toml
 fi
