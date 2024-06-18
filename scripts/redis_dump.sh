ba0aed748e79a2727fdc61989dfea0e8c20be455
Claudio Cicconetti <c.cicconetti@iit.cnr.it>
2024-06-18 06:06:03 +0000

    Add REDIS_PORT to redis-cli

--- scripts/redis_dump.sh
+++ scripts/redis_dump.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-for i in $(redis-cli --scan --pattern '*' | sort) ; do
-	type=$(redis-cli type $i)
+if [ "$REDIS_PORT" == "" ] ; then
+	REDIS_PORT=6379
+fi
+
+for i in $(redis-cli -p $REDIS_PORT --scan --pattern '*' | sort) ; do
+	type=$(redis-cli -p $REDIS_PORT type $i)
 	if [ "$type" == "list" ] ; then
-		data=$(redis-cli --raw lrange $i 0 -1 | tr '\n' ' ')
+		data=$(redis-cli -p $REDIS_PORT --raw lrange $i 0 -1 | tr '\n' ' ')
 	elif [ "$type" == "string" ] ; then
-		data=$(redis-cli get $i)
+		data=$(redis-cli -p $REDIS_PORT get $i)
 		json=$(python3 -m json.tool <<< "$data")
 		if [ $? -eq 0 ] ; then
 			data=$json
