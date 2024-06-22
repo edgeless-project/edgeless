6ee4cb32c7230b3fe507ccebb929afe8fdae9782
Claudio Cicconetti <c.cicconetti@iit.cnr.it>
2024-06-22 14:20:37 +0200

    Add system test to check function migration via Redis intents

--- edgeless_systemtests/src/lib.rs
+++ edgeless_systemtests/src/lib.rs
@@ -5,10 +5,96 @@
 mod tests {
     // use super::*;
 
+    fn string_to_instance_id(val: &str) -> edgeless_api::function_instance::InstanceId {
+        let tokens: Vec<&str> = val.split(' ').collect();
+        if tokens.len() == 4 {
+            let node_id = match uuid::Uuid::from_str(&tokens[1][0..tokens[1].len() - 1]) {
+                Ok(val) => val,
+                Err(_) => uuid::Uuid::nil(),
+            };
+            let function_id = match uuid::Uuid::from_str(&tokens[3][0..tokens[3].len() - 1]) {
+                Ok(val) => val,
+                Err(_) => uuid::Uuid::nil(),
+            };
+            edgeless_api::function_instance::InstanceId { node_id, function_id }
+        } else {
+            edgeless_api::function_instance::InstanceId::none()
+        }
+    }
+
+    // Data structure clone of ActiveInstance, which can be deserialized.
+    #[derive(Clone, serde::Deserialize)]
+    pub enum ActiveInstanceClone {
+        // 0: request
+        // 1: [ (node_id, int_fid) ]
+        Function(edgeless_api::function_instance::SpawnFunctionRequest, Vec<String>),
+
+        // 0: request
+        // 1: (node_id, int_fid)
+        Resource(edgeless_api::resource_configuration::ResourceInstanceSpecification, String),
+    }
+
+    impl ActiveInstanceClone {
+        fn node_id(&self) -> edgeless_api::function_instance::NodeId {
+            string_to_instance_id(match self {
+                ActiveInstanceClone::Function(_, instances) => match instances.first() {
+                    Some(val) => val,
+                    None => return uuid::Uuid::nil(),
+                },
+                ActiveInstanceClone::Resource(_, instance) => instance,
+            })
+            .node_id
+        }
+    }
+
+    use std::str::FromStr;
+
     use edgeless_api::controller::ControllerAPI;
     use edgeless_api::workflow_instance::WorkflowInstanceAPI;
+    use redis::Commands;
 
-    async fn setup(num_domains: u32, num_nodes_per_domain: u32) -> (Vec<futures::future::AbortHandle>, Box<(dyn WorkflowInstanceAPI)>) {
+    fn redis_node_ids(connection: &mut redis::Connection) -> Vec<edgeless_api::function_instance::NodeId> {
+        let mut uuids = vec![];
+        for node_key in connection.keys::<&str, Vec<String>>("node:capabilities:*").unwrap_or(vec![]) {
+            let tokens: Vec<&str> = node_key.split(':').collect();
+            assert_eq!(tokens.len(), 3);
+            if let Ok(uuid) = edgeless_api::function_instance::NodeId::parse_str(tokens[2]) {
+                uuids.push(uuid);
+            }
+        }
+        uuids
+    }
+
+    fn redis_instances(
+        connection: &mut redis::Connection,
+    ) -> std::collections::HashMap<edgeless_api::function_instance::ComponentId, ActiveInstanceClone> {
+        let mut instance_ids = vec![];
+        for instance_key in connection.keys::<&str, Vec<String>>("instance:*").unwrap_or(vec![]) {
+            let tokens: Vec<&str> = instance_key.split(':').collect();
+            assert_eq!(tokens.len(), 2);
+            if let Ok(uuid) = edgeless_api::function_instance::NodeId::parse_str(tokens[1]) {
+                instance_ids.push(uuid);
+            }
+        }
+        let mut instances = std::collections::HashMap::new();
+        for instance_id in instance_ids {
+            if let Ok(val) = connection.get::<String, String>(format!("instance:{}", instance_id.to_string())) {
+                if let Ok(val) = serde_json::from_str(&val) {
+                    instances.insert(instance_id, val);
+                }
+                if let Err(err) = serde_json::from_str::<ActiveInstanceClone>(&val) {
+                    println!("{}: {}", val, err);
+                }
+            }
+        }
+        instances
+    }
+
+    async fn setup(
+        num_domains: u32,
+        num_nodes_per_domain: u32,
+        redis_url: Option<&str>,
+    ) -> (Vec<futures::future::AbortHandle>, Box<(dyn WorkflowInstanceAPI)>) {
         assert!(num_domains > 0);
         assert!(num_nodes_per_domain > 0);
 
@@ -44,16 +130,28 @@ mod tests {
                     invocation_url_announced: "".to_string(),
                 },
                 baseline: edgeless_orc::EdgelessOrcBaselineSettings {
-                    orchestration_strategy: edgeless_orc::OrchestrationStrategy::Random,
+                    orchestration_strategy: edgeless_orc::OrchestrationStrategy::RoundRobin,
                     keep_alive_interval_secs: 1,
                 },
-                proxy: edgeless_orc::EdgelessOrcProxySettings {
-                    proxy_type: "None".to_string(),
-                    redis_url: None,
+                proxy: match redis_url {
+                    None => edgeless_orc::EdgelessOrcProxySettings {
+                        proxy_type: "None".to_string(),
+                        redis_url: None,
+                    },
+                    Some(url) => edgeless_orc::EdgelessOrcProxySettings {
+                        proxy_type: "Redis".to_string(),
+                        redis_url: Some(url.to_string()),
+                    },
                 },
-                collector: edgeless_orc::EdgelessOrcCollectorSettings {
-                    collector_type: "None".to_string(),
-                    redis_url: None,
+                collector: match redis_url {
+                    None => edgeless_orc::EdgelessOrcCollectorSettings {
+                        collector_type: "None".to_string(),
+                        redis_url: None,
+                    },
+                    Some(url) => edgeless_orc::EdgelessOrcCollectorSettings {
+                        collector_type: "Redis".to_string(),
+                        redis_url: Some(url.to_string()),
+                    },
                 },
             }));
             tokio::spawn(task);
@@ -138,7 +236,7 @@ mod tests {
         let _ = env_logger::try_init();
 
         // Create the EDGELESS system.
-        let (handles, mut client) = setup(1, 1).await;
+        let (handles, mut client) = setup(1, 1, None).await;
 
         assert!(wf_list(&mut client).await.is_empty());
 
@@ -204,7 +302,7 @@ mod tests {
         let _ = env_logger::try_init();
 
         // Create the EDGELESS system.
-        let (handles, mut client) = setup(1, 3).await;
+        let (handles, mut client) = setup(1, 3, None).await;
 
         assert!(wf_list(&mut client).await.is_empty());
 
@@ -218,7 +316,7 @@ mod tests {
             }
         };
 
-        // Create 10 workflows
+        // Create workflows
         let mut workflow_ids = vec![];
         cleanup();
         for workflow_i in 0..num_workflows {
@@ -307,6 +405,177 @@ mod tests {
                 Ok(_) => {}
                 Err(err) => panic!("could not stop the workflow: {}", err),
             }
+        }
+        assert!(wf_list(&mut client).await.is_empty());
+
+        cleanup();
+        terminate(handles)
+    }
+
+    #[tokio::test(flavor = "multi_thread", worker_threads = 1)]
+    #[serial_test::serial]
+    async fn system_test_orchestration_intent_migration_redis() -> anyhow::Result<()> {
+        let _ = env_logger::try_init();
+
+        // Skip the test if there is no local Redis listening on default port.
+        let mut redis_connection = match redis::Client::open("redis://localhost:6379") {
+            Ok(client) => match client.get_connection() {
+                Ok(connection) => connection,
+                Err(_) => {
+                    println!("the test cannot be run because there is no Redis reachable on localhost at port 6379");
+                    return Ok(());
+                }
+            },
+            Err(_) => {
+                println!("the test cannot be run because there is no Redis reachable on localhost at port 6379");
+                return Ok(());
+            }
+        };
+
+        // Create an EDGELESS system with a single domain and two nodes.
+        let (handles, mut client) = setup(1, 2, Some("redis://127.0.0.1:6379")).await;
+
+        // Check that in the Redis there are two regular nodes, in addition to
+        // the one for metrics collection in the orchestrator.
+        let node_uuids = redis_node_ids(&mut redis_connection);
+        assert_eq!(node_uuids.len(), 1 + 2);
+
+        // Check that there is no workflow through the client.
+        assert!(wf_list(&mut client).await.is_empty());
+
+        // Clean-up closures.
+        let removeme_filename = || format!("removeme.log");
+        let cleanup = || {
+            let _ = std::fs::remove_file(removeme_filename());
+        };
+
+        // Create one workflow
+        let workflow_id;
+        cleanup();
+        let res = client
+            .start(edgeless_api::workflow_instance::SpawnWorkflowRequest {
+                workflow_functions: vec![
+                    edgeless_api::workflow_instance::WorkflowFunction {
+                        name: "f1".to_string(),
+                        function_class_specification: fixture_spec(),
+                        output_mapping: std::collections::HashMap::from([
+                            ("out1".to_string(), "f2".to_string()),
+                            ("out2".to_string(), "f3".to_string()),
+                        ]),
+                        annotations: std::collections::HashMap::from([("init-payload".to_string(), "8".to_string())]),
+                    },
+                    edgeless_api::workflow_instance::WorkflowFunction {
+                        name: "f2".to_string(),
+                        function_class_specification: fixture_spec(),
+                        output_mapping: std::collections::HashMap::from([("log".to_string(), "log".to_string())]),
+                        annotations: std::collections::HashMap::new(),
+                    },
+                    edgeless_api::workflow_instance::WorkflowFunction {
+                        name: "f3".to_string(),
+                        function_class_specification: fixture_spec(),
+                        output_mapping: std::collections::HashMap::from([("log".to_string(), "log".to_string())]),
+                        annotations: std::collections::HashMap::new(),
+                    },
+                ],
+                workflow_resources: vec![],
+                annotations: std::collections::HashMap::new(),
+            })
+            .await;
+        workflow_id = Some(match res {
+            Ok(response) => match &response {
+                edgeless_api::workflow_instance::SpawnWorkflowResponse::ResponseError(err) => {
+                    panic!("workflow rejected: {}", err)
+                }
+                edgeless_api::workflow_instance::SpawnWorkflowResponse::WorkflowInstance(val) => {
+                    assert_eq!(3, val.domain_mapping.len());
+                    for i in 0..3 {
+                        assert_eq!(format!("f{}", i + 1), val.domain_mapping[i].name);
+                        assert_eq!("domain-0", val.domain_mapping[i].domain_id);
+                    }
+                    val.workflow_id.clone()
+                }
+            },
+            Err(err) => panic!("could not start the workflow: {}", err),
+        });
+
+        // Check that the client now shows one workflow.
+        assert_eq!(1, wf_list(&mut client).await.len());
+
+        // Find the logical identifiers of the functions and how the instances
+        // were mapped to nodes.
+        let mut instances = std::collections::HashMap::new();
+        for _ in 0..100 {
+            instances = redis_instances(&mut redis_connection);
+            if instances.len() == 3 {
+                break;
+            } else {
+                tokio::time::sleep(tokio::time::Duration::from_millis(100)).await;
+            }
+        }
+        let mut instances_to_nodes = std::collections::HashMap::new();
+        for (logical_fid, instance) in instances {
+            println!("before function {} -> node {}", logical_fid, instance.node_id());
+            instances_to_nodes.insert(logical_fid, instance.node_id());
+        }
+
+        // Find the nodes with assigned functions.
+        let mut nodes_with_functions = std::collections::HashSet::new();
+        for (_logical_fid, node_id) in &instances_to_nodes {
+            nodes_with_functions.insert(node_id.clone());
+        }
+        assert_eq!(nodes_with_functions.len(), 2);
+
+        // Add intents to migrate the function instances.
+        let other = |x: &edgeless_api::function_instance::NodeId| {
+            if let Some(val) = nodes_with_functions.iter().find(|y| x != *y) {
+                *val
+            } else {
+                uuid::Uuid::nil()
+            }
+        };
+        for (logical_fid, node_id) in &instances_to_nodes {
+            let key = format!("intent:migrate:{}", logical_fid);
+            let _ = redis_connection.set::<&str, &str, usize>(&key, &other(node_id).to_string());
+            let _ = redis_connection.lpush::<&str, &str, String>("intents", &key);
+        }
+
+        // Wait until the policy is implemented.
+        for _ in 0..100 {
+            let new_instances = redis_instances(&mut redis_connection);
+            assert_eq!(new_instances.len(), instances_to_nodes.len());
+
+            let mut not_done = false;
+            for (logical_fid, node_id) in &instances_to_nodes {
+                if let Some(new_instance) = new_instances.get(logical_fid) {
+                    let new_node = new_instance.node_id();
+                    if new_node != other(node_id) {
+                        not_done = true;
+                        break;
+                    }
+                }
+            }
+
+            if !not_done {
+                break;
+            } else {
+                tokio::time::sleep(tokio::time::Duration::from_millis(100)).await;
+            }
+        }
+
+        // Print new mapping
+        instances = redis_instances(&mut redis_connection);
+        for (logical_fid, instance) in instances {
+            println!("after function {} -> node {}", logical_fid, instance.node_id());
+        }
+
+        // Check that the intents have been cleared.
+        assert!(redis_connection.keys::<&str, Vec<String>>("intents").unwrap().is_empty());
+        assert!(redis_connection.keys::<&str, Vec<String>>("intent:*").unwrap().is_empty());
+
+        // Stop the workflows
+        match client.stop(workflow_id.unwrap()).await {
+            Ok(_) => {}
+            Err(err) => panic!("could not stop the workflow: {}", err),
         }
         assert!(wf_list(&mut client).await.is_empty());
 
