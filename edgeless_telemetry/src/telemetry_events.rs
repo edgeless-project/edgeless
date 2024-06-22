b5f2db9a459d716e84a5e8aadc0a9c08cb87ed3c
pantoni <p.antoniou@aegisresearch.eu>
2024-06-22 09:48:06 +0000

    telemetry events update

--- edgeless_telemetry/src/telemetry_events.rs
+++ edgeless_telemetry/src/telemetry_events.rs
@@ -143,6 +143,7 @@ impl TelemetryProcessor {
                 let inner = TelemetryProcessorInner {
                     processing_chain: vec![
                         Box::new(crate::prometheus_target::PrometheusEventTarget::new(&format!("{}:{}", &ip, port)).await),
+                        // elastic here
                         Box::new(EventLogger {}),
                     ],
                     receiver,
