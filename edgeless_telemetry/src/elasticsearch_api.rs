1e655b024f2479a2b7947283b43885e67d65d706
pantoni <p.antoniou@aegisresearch.eu>
2024-06-24 22:05:30 +0000

    telemetry events asynchronous calls

--- edgeless_telemetry/src/elasticsearch_api.rs
+++ edgeless_telemetry/src/elasticsearch_api.rs
@@ -1,16 +1,17 @@
+use anyhow::{anyhow, Result};
+use async_trait::async_trait;
 use chrono::{DateTime, Utc};
 use elasticsearch::indices::IndicesCreateParts;
 use elasticsearch::{
     auth::Credentials, http::transport::SingleNodeConnectionPool, http::transport::TransportBuilder, Elasticsearch, IndexParts, SearchParts,
 };
-
 use serde::Deserialize;
 use serde_json::json;
 use serde_json::Value;
 use std::fs;
-use std::sync::atomic::{AtomicUsize, Ordering};
 use url::Url;
 use uuid::Uuid;
+
 #[derive(Debug)]
 pub enum IndexType {
     Runtime,
@@ -27,28 +28,11 @@ fn read_creds_from_file(file_path: &str) -> Creds {
     let creds: Creds = serde_json::from_str(&contents).expect("Failed to parse JSON");
     creds
 }
-//used to generate index UUID
-pub struct IdGenerator {
-    counter: AtomicUsize,
-}
 
-impl IdGenerator {
-    pub fn new() -> Self {
-        IdGenerator {
-            counter: AtomicUsize::new(0),
-        }
-    }
-
-    pub fn increment_counter(&self) -> String {
-        let count = self.counter.fetch_add(1, Ordering::SeqCst);
-        let uuid = Uuid::new_v4();
-        format!("{}_{}", count, uuid)
-    }
-}
-/// Establish connection to specified ES endpoint.
-/// # Returns
-/// A Result indicating success or failure.
-pub fn es_create_client() -> Result<Elasticsearch, Box<dyn std::error::Error>> {
+// / Establish connection to specified ES endpoint.
+// / # Returns
+// / A Result indicating success or failure.
+pub async fn es_create_client() -> Result<Elasticsearch> {
     //define ES endpoint configs
     //read credentials from file
     let file_path = "/workspaces/edgeless/es_creds.json";
@@ -60,13 +44,86 @@ pub fn es_create_client() -> Result<Elasticsearch, Box<dyn std::error::Error>> {
     let credentials = Credentials::Basic(creds.username, creds.password);
     let conn_pool = SingleNodeConnectionPool::new(url);
     let transport = TransportBuilder::new(conn_pool).auth(credentials).build()?;
-    //TODO: Return only the client and not the connection response
-    // let client = Elasticsearch::new(transport);
-    // client
-    Ok(Elasticsearch::new(transport))
-}
-///Perform a check when the create client is called to check the result
+    let client = Elasticsearch::new(transport);
+    // Perform a ping request to ensure the connection is successful
+    let response = client.ping().send().await;
 
+    match response {
+        Ok(_) => Ok(client),
+        Err(err) => Err(anyhow!("Failed to connect to Elasticsearch: {}", err)),
+    }
+}
+
+#[async_trait]
+pub trait ElasticsearchClient {
+    fn write_event(
+        &self,
+        event: &crate::telemetry_events::TelemetryEvent,
+        tags: &std::collections::BTreeMap<String, String>,
+    ) -> Result<(), elasticsearch::Error>;
+}
+
+pub struct ESClient {
+    client: Elasticsearch,
+}
+/// Establish connection to specified ES endpoint.
+/// # Returns
+/// A Result indicating success or failure.
+impl ESClient {
+    pub async fn new() -> anyhow::Result<Self> {
+        let file_path = "/workspaces/edgeless/es_creds.json";
+        let creds = read_creds_from_file(file_path);
+
+        let url = Url::parse(&creds.url)?;
+        let credentials = Credentials::Basic(creds.username, creds.password);
+        let conn_pool = SingleNodeConnectionPool::new(url);
+        let transport = TransportBuilder::new(conn_pool).auth(credentials).build()?;
+        let client = Elasticsearch::new(transport);
+
+        let response = client.ping().send().await?;
+        if response.status_code().is_success() {
+            log::info!("Elasticsearch Connected");
+            Ok(Self { client })
+        } else {
+            Err(anyhow::anyhow!("Failed to connect to Elasticsearch"))
+        }
+    }
+    pub fn write_event(
+        &mut self,
+        event: &crate::telemetry_events::TelemetryEvent,
+        event_tags: &std::collections::BTreeMap<String, String>,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        let file_path = "/workspaces/edgeless/es_creds.json";
+        let creds = read_creds_from_file(file_path);
+        //synchronous Elasticsearch operations
+        log::info!("ELASTICSEARCH WRITE EVENT");
+        let id = Uuid::new_v4().to_string();
+        let document = json!({
+            "event": format!("{:?}", event),
+            "tags": event_tags,
+        });
+        let body = format!(
+            "{}\n{}\n",
+            json!({ "index": { "_index": "edgeless_runtime", "_id": id } }).to_string(),
+            document.to_string()
+        );
+        let response = self
+            .client
+            .send(creds.url)
+            .header("Content-Type", "application/x-ndjson")
+            .basic_auth(creds.username, Some(creds.password))
+            .body(body)
+            .send()?;
+
+        if response.status().is_success() {
+            println!("Bulk operation succeeded");
+            Ok(())
+        } else {
+            println!("Bulk operation failed: {:?}", response);
+            Err("Bulk operation failed".into()) // Replace with appropriate error handling
+        }
+    }
+}
 /// Creates an Elasticsearch index with a specified mapping.
 /// # Arguments
 /// * `client_result` - the Elasticsearch client or an error.
@@ -135,20 +192,16 @@ pub async fn es_create_index(client: &Elasticsearch, index_type: IndexType) -> R
 /// # Returns
 /// A Result indicating success or failure.
 
-pub async fn es_write_to_index(
-    client: &Elasticsearch,
-    data: Value,
-    index_type: IndexType,
-    id_generator: &IdGenerator,
-) -> Result<(), Box<dyn std::error::Error>> {
-    let id = id_generator.increment_counter(); //send data to ES endpoint via POST
+pub async fn es_write_to_index(client: &Elasticsearch, data: Value, index_type: IndexType) -> Result<(), Box<dyn std::error::Error>> {
+    // log::info!("called");
+    let id = Uuid::new_v4().to_string(); //send data to ES endpoint via POST
     let index_response = client
         .index(IndexParts::IndexId(get_index_name(index_type), &id))
         .body(data)
         .send()
         .await?;
 
-    log::info!("Response Status Code: {}", index_response.status_code());
+    log::info!("Write to index: Response Status Code: {}", index_response.status_code());
     Ok(())
 }
 
