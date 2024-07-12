4297f4d43dc6c4f32a95e190c63089355006e70c
Raphael Hetzel <hetzel@in.tum.de>
2024-07-12 11:54:13 +0000

    Improve COAP response handling

--- edgeless_api/src/coap_impl/mod.rs
+++ edgeless_api/src/coap_impl/mod.rs
@@ -2,6 +2,8 @@
 // SPDX-FileCopyrightText: © 2023 Claudio Cicconetti <c.cicconetti@iit.cnr.it>
 // SPDX-License-Identifier: MIT
 
+use futures::FutureExt;
+
 pub mod agent;
 pub mod function_instance;
 pub mod invocation;
@@ -13,24 +15,88 @@ pub mod resource_configuration;
 #[derive(Clone)]
 pub struct CoapClient {
     inner: std::sync::Arc<tokio::sync::Mutex<CoapClientInner>>,
+    outgoing_sender: tokio::sync::mpsc::UnboundedSender<Vec<u8>>,
+    network_task_abort_handle: Option<std::sync::Arc<tokio::task::AbortHandle>>,
 }
 
 struct CoapClientInner {
-    sock: tokio::net::UdpSocket,
     endpoint: std::net::SocketAddrV4,
     next_token: u8,
+    active_requests: std::collections::HashMap<u8, tokio::sync::oneshot::Sender<Result<Vec<u8>, Vec<u8>>>>,
 }
 
 impl CoapClient {
     pub async fn new(peer: std::net::SocketAddrV4) -> Self {
         let sock = tokio::net::UdpSocket::bind("0.0.0.0:0").await.unwrap();
 
+        let inner = std::sync::Arc::new(tokio::sync::Mutex::new(CoapClientInner {
+            endpoint: peer.clone(),
+            next_token: 0,
+            active_requests: std::collections::HashMap::new(),
+        }));
+
+        let (sender, receiver) = tokio::sync::mpsc::unbounded_channel::<Vec<u8>>();
+
+        let inner_clone = inner.clone();
+        let task_handle = tokio::task::spawn(Self::network_task(sock, inner_clone, peer, receiver));
+
         CoapClient {
-            inner: std::sync::Arc::new(tokio::sync::Mutex::new(CoapClientInner {
-                sock,
-                endpoint: peer,
-                next_token: 0,
-            })),
+            inner: inner.clone(),
+            outgoing_sender: sender,
+            network_task_abort_handle: Some(std::sync::Arc::new(task_handle.abort_handle())),
+        }
+    }
+
+    async fn network_task(
+        sock: tokio::net::UdpSocket,
+        inner: std::sync::Arc<tokio::sync::Mutex<CoapClientInner>>,
+        endpoint: std::net::SocketAddrV4,
+        mut outgoing_receiver: tokio::sync::mpsc::UnboundedReceiver<Vec<u8>>,
+    ) {
+        loop {
+            let mut buffer = vec![0 as u8; 5000];
+
+            futures::select! {
+                msg = sock.recv_from(&mut buffer).fuse() => {
+                    log::debug!("Receive");
+                    if let Ok((len, sender)) = msg {
+                        if sender != std::net::SocketAddr::V4(endpoint) {
+                            log::debug!("Wrong Sender");
+                            continue;
+                        }
+
+                        let (res, response_token) = edgeless_api_core::coap_mapping::CoapDecoder::decode(&buffer[..len]).unwrap();
+                        match res {
+                            edgeless_api_core::coap_mapping::CoapMessage::Response(response_data, ok) => {
+                                if let Some(responder) = inner.lock().await.active_requests.remove(&response_token) {
+                                    if responder.send(
+                                        match ok {
+                                            true => Ok(Vec::from(response_data)),
+                                            false => Err(Vec::from(response_data)),
+                                        }
+                                    ).is_err() {
+                                        log::warn!("Could not send response");
+                                    }
+                                } else {
+                                    log::debug!("No interested receiver for message");
+                                }
+                            }
+                            _ => {
+                                log::info!("Received wrong message");
+                            }
+                        }
+                    } else {
+                        log::warn!("Socket Receive Error");
+                    }
+
+                },
+                outgoing = outgoing_receiver.recv().fuse() => {
+                    if let Some(outgoing) = outgoing {
+                        log::debug!("COAP: {:?} {:?}", endpoint, outgoing);
+                        sock.send_to(&outgoing, endpoint).await.unwrap();
+                    }
+                }
+            }
         }
     }
 
@@ -38,46 +104,50 @@ impl CoapClient {
         &mut self,
         encode_request: impl Fn(u8, std::net::SocketAddrV4, &mut [u8]) -> ((&mut [u8], std::net::SocketAddrV4), &mut [u8]),
     ) -> Result<Vec<u8>, Vec<u8>> {
-        let mut lck = self.inner.lock().await;
+        let (token, endpoint, mut receiver) = {
+            let mut lck = self.inner.lock().await;
+            let token = lck.next_token;
+            (lck.next_token, _) = lck.next_token.overflowing_add(1);
 
-        let mut buffer = vec![0 as u8; 5000];
+            let (sender, receiver) = tokio::sync::oneshot::channel::<Result<Vec<u8>, Vec<u8>>>();
 
-        let token = lck.next_token;
-        (lck.next_token, _) = lck.next_token.overflowing_add(1);
+            lck.active_requests.insert(token, sender);
+            (token, lck.endpoint, receiver)
+        };
 
-        for _ in 0..3 {
-            let ((packet, addr), _tail) = encode_request(token, lck.endpoint, &mut buffer);
-            lck.sock.send_to(&packet, addr).await.unwrap();
-
-            let res = tokio::time::timeout(core::time::Duration::from_millis(500), lck.sock.recv_from(&mut buffer)).await;
-
-            if res.is_err() {
-                log::debug!("reached receive timeout");
-                continue;
+        for i in 0..3 {
+            let mut buffer = vec![0 as u8; 5000];
+            let ((packet, _addr), _tail) = encode_request(token, endpoint, &mut buffer);
+            if self.outgoing_sender.send(Vec::from(packet)).is_err() {
+                log::warn!("Sender could not send on iteration {}", i);
             }
 
-            let (size, sender) = res.unwrap().unwrap();
-
-            if sender != std::net::SocketAddr::V4(lck.endpoint) {
-                continue;
-            }
-            let (res, response_token) = edgeless_api_core::coap_mapping::CoapDecoder::decode(&buffer[..size]).unwrap();
+            let res = tokio::time::timeout(core::time::Duration::from_millis(500), &mut receiver).await;
             match res {
-                edgeless_api_core::coap_mapping::CoapMessage::Response(response_data, ok) => {
-                    if response_token == token {
-                        match ok {
-                            true => return Ok(Vec::from(response_data)),
-                            false => return Err(Vec::from(response_data)),
-                        }
-                    } else {
-                        log::info!("received wrong token: wanted {}, got: {}", token, response_token);
+                Ok(reply) => {
+                    if let Ok(val) = reply {
+                        return val;
                     }
                 }
-                _ => {
-                    log::info!("received wrong message");
+                Err(_timeout) => {
+                    log::debug!("reached receive timeout");
+                    continue;
                 }
             }
         }
-        Err(Vec::from("client cimeout".as_bytes()))
+        Err(Vec::from("client timeout".as_bytes()))
+    }
+}
+
+impl Drop for CoapClient {
+    fn drop(&mut self) {
+        if let Some(abort_handle) = self.network_task_abort_handle.take() {
+            if let Some(abort_handle) = std::sync::Arc::into_inner(abort_handle) {
+                log::info!("Network task stopped.");
+                abort_handle.abort();
+            }
+        } else {
+            log::warn!("Network task handle removed before drop.");
+        }
     }
 }
