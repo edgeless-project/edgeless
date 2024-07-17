30638a27aca5469206e2d679c813c53d4af95b9b
Claudio Cicconetti <c.cicconetti@iit.cnr.it>
2024-07-17 16:59:23 +0000

    Fix http_extractor, which originally did not extract the body from the HTTP request (facepalm)

--- functions/http_extractor/src/lib.rs
+++ functions/http_extractor/src/lib.rs
@@ -10,15 +10,21 @@ impl EdgeFunction for HttpExtractor {
     fn handle_cast(_src: InstanceId, _encoded_message: &[u8]) {}
 
     fn handle_call(_src: InstanceId, encoded_message: &[u8]) -> CallRet {
-        let resp = EdgelessHTTPResponse {
-            status: 200,
-            body: None,
-            headers: std::collections::HashMap::<String, String>::new(),
-        };
+        if let Some(body) = edgeless_http::request_from_string(core::str::from_utf8(encoded_message).unwrap())
+            .unwrap()
+            .body
+        {
+            cast("out", &body);
+        }
 
-        cast("out", encoded_message);
-
-        CallRet::Reply(OwnedByteBuff::new_from_slice(edgeless_http::response_to_string(&resp).as_bytes()))
+        CallRet::Reply(OwnedByteBuff::new_from_slice(
+            edgeless_http::response_to_string(&EdgelessHTTPResponse {
+                status: 200,
+                body: None,
+                headers: std::collections::HashMap::<String, String>::new(),
+            })
+            .as_bytes(),
+        ))
     }
 
     fn handle_init(_payload: Option<&[u8]>, _serialized_state: Option<&[u8]>) {}
