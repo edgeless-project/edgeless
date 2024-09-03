07f3e03e0ed9cb1aac99cd9d4fa9edfcfb3b3174
Claudio Cicconetti <c.cicconetti@iit.cnr.it>
2024-09-03 14:17:13 +0200

    Update controller API: now providing the client with info about the identifier of functions/resources deployed on an orchestration domain

--- edgeless_api/src/orc.rs
+++ edgeless_api/src/orc.rs
@@ -1,6 +1,7 @@
 // SPDX-FileCopyrightText: © 2023 Technical University of Munich, Chair of Connected Mobility
 // SPDX-License-Identifier: MIT
-pub type DomainManagedInstanceId = uuid::Uuid;
+
+pub use edgeless_api_core::instance_id::*;
 
 pub trait OrchestratorAPI: Send {
     fn function_instance_api(&mut self) -> Box<dyn crate::function_instance::FunctionInstanceAPI<DomainManagedInstanceId>>;
