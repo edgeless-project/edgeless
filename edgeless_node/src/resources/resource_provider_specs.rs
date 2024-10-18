cef4e2ff04167ac0d215129e998e403eec99b03a
Claudio Cicconetti <c.cicconetti@iit.cnr.it>
2024-10-18 15:17:52 +0200

    Minor refactor the JSON output of a node's available resources

--- edgeless_node/src/resources/resource_provider_specs.rs
+++ edgeless_node/src/resources/resource_provider_specs.rs
@@ -7,3 +7,22 @@ pub trait ResourceProviderSpecs {
     fn configurations(&self) -> std::collections::HashMap<String, String>;
     fn version(&self) -> String;
 }
+
+#[derive(serde::Serialize)]
+pub struct ResourceProviderSpecOutput {
+    class_type: String,
+    version: String,
+    outputs: Vec<String>,
+    configurations: std::collections::HashMap<String, String>,
+}
+
+impl dyn ResourceProviderSpecs {
+    pub fn to_output(&self) -> ResourceProviderSpecOutput {
+        ResourceProviderSpecOutput {
+            class_type: self.class_type(),
+            version: self.version(),
+            outputs: self.outputs(),
+            configurations: self.configurations(),
+        }
+    }
+}
