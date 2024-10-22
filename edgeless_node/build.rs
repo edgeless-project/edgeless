07e16fe44419f3504f4b7c5533fe2b6555dee502
Álvaro Curto Merino <alvaro.curtomerino@telefonica.com>
2024-10-22 12:19:20 +0200

    cargo fmt --check

--- edgeless_node/build.rs
+++ edgeless_node/build.rs
@@ -2,13 +2,13 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Add flag enabling optional proto3 in tonic_build
     tonic_build::configure().protoc_arg("--experimental_allow_proto3_optional").compile(
-            &[
-                "src/resources/dda/proto/com.proto",
-                "src/resources/dda/proto/state.proto",
-                "src/resources/dda/proto/store.proto"
-            ],
-            &["src/resources/dda/proto"]
-        )?;
+        &[
+            "src/resources/dda/proto/com.proto",
+            "src/resources/dda/proto/state.proto",
+            "src/resources/dda/proto/store.proto"
+        ],
+        &["src/resources/dda/proto"]
+    )?;
 
     // recommended method for generating rust bindings for gRPC for DDA
     // tonic_build::compile_protos("src/resources/dda/proto/com.proto")?;
