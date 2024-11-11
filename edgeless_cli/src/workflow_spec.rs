3360c1429d7db045d1ab1bdece1abc91d39328cd
Claudio Cicconetti <c.cicconetti@iit.cnr.it>
2024-11-11 17:59:25 +0100

    Repo housekeeping: update authors in cargo manifests

--- edgeless_cli/src/workflow_spec.rs
+++ edgeless_cli/src/workflow_spec.rs
@@ -1,6 +1,6 @@
-// SPDX-FileCopyrightText: © 2023 Technical University of Munich, Chair of
-// Connected Mobility SPDX-FileCopyrightText: © 2023 Claudio Cicconetti
-// <c.cicconetti@iit.cnr.it> SPDX-License-Identifier: MIT
+// SPDX-FileCopyrightText: © 2023 Technical University of Munich, Chair of Connected Mobility
+// SPDX-FileCopyrightText: © 2023 Claudio Cicconetti <c.cicconetti@iit.cnr.it>
+// SPDX-License-Identifier: MIT
 
 // NOTE: Serialize derived for all structs to silence the compiler because of
 // JsonSchema
