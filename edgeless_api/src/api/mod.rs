56f988951f54eb91105b7abd76d966ac77421e01
Claudio Cicconetti <c.cicconetti@iit.cnr.it>
2024-11-26 08:32:03 +0100

    edgeless_api code restructuring: move high-level API traits into dedicated api namespace, to avoid mixing them with lower level APIs

---
+++ edgeless_api/src/api/mod.rs
@@ -0,0 +1,8 @@
+// SPDX-FileCopyrightText: © 2024 Claudio Cicconetti <c.cicconetti@iit.cnr.it>
+// SPDX-License-Identifier: MIT
+
+pub mod agent;
+pub mod container_function;
+pub mod container_runtime;
+pub mod controller;
+pub mod orc;
