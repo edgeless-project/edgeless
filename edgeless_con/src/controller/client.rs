857ae7da3ba32d7101896b288015fdc4d8d3d821
Claudio Cicconetti <c.cicconetti@iit.cnr.it>
2024-11-29 12:45:23 +0100

    edgeless_controller: WIP implementation of dynamic cluster formation
    edgeless_cli: add domain commands

--- edgeless_con/src/controller/client.rs
+++ edgeless_con/src/controller/client.rs
@@ -38,20 +38,18 @@ impl edgeless_api::workflow_instance::WorkflowInstanceAPI for ControllerWorkflow
     ) -> anyhow::Result<edgeless_api::workflow_instance::SpawnWorkflowResponse> {
         let (reply_sender, reply_receiver) =
             tokio::sync::oneshot::channel::<anyhow::Result<edgeless_api::workflow_instance::SpawnWorkflowResponse>>();
-        match self.sender.send(super::ControllerRequest::Start(request.clone(), reply_sender)).await {
-            Ok(_) => {}
-            Err(_) => return Err(anyhow::anyhow!("Controller Channel Error")),
+        if let Err(err) = self.sender.send(super::ControllerRequest::Start(request.clone(), reply_sender)).await {
+            anyhow::bail!("Controller Channel Error: {}", err);
         }
-        let reply = reply_receiver.await;
-        match reply {
+        match reply_receiver.await {
             Ok(ret) => ret,
-            Err(_) => Err(anyhow::anyhow!("Controller Channel Error")),
+            Err(err) => Err(anyhow::anyhow!("Controller Channel Error: {}", err)),
         }
     }
     async fn stop(&mut self, id: edgeless_api::workflow_instance::WorkflowId) -> anyhow::Result<()> {
         match self.sender.send(super::ControllerRequest::Stop(id)).await {
             Ok(_) => Ok(()),
-            Err(_) => Err(anyhow::anyhow!("Controller Channel Error")),
+            Err(err) => Err(anyhow::anyhow!("Controller Channel Error: {}", err)),
         }
     }
     async fn list(
@@ -60,14 +58,27 @@ impl edgeless_api::workflow_instance::WorkflowInstanceAPI for ControllerWorkflow
     ) -> anyhow::Result<Vec<edgeless_api::workflow_instance::WorkflowInstance>> {
         let (reply_sender, reply_receiver) =
             tokio::sync::oneshot::channel::<anyhow::Result<Vec<edgeless_api::workflow_instance::WorkflowInstance>>>();
-        match self.sender.send(super::ControllerRequest::List(id.clone(), reply_sender)).await {
-            Ok(_) => {}
-            Err(_) => return Err(anyhow::anyhow!("Controller Channel Error")),
+        if let Err(err) = self.sender.send(super::ControllerRequest::List(id.clone(), reply_sender)).await {
+            anyhow::bail!("Controller Channel Error: {}", err);
         }
-        let reply = reply_receiver.await;
-        match reply {
+        match reply_receiver.await {
             Ok(ret) => ret,
-            Err(_) => Err(anyhow::anyhow!("Controller Channel Error")),
+            Err(err) => Err(anyhow::anyhow!("Controller Channel Error: {}", err)),
+        }
+    }
+    async fn domains(
+        &mut self,
+        domain_id: String,
+    ) -> anyhow::Result<std::collections::HashMap<String, edgeless_api::domain_registration::DomainCapabilities>> {
+        let (reply_sender, reply_receiver) = tokio::sync::oneshot::channel::<
+            anyhow::Result<std::collections::HashMap<String, edgeless_api::domain_registration::DomainCapabilities>>,
+        >();
+        if let Err(err) = self.sender.send(super::ControllerRequest::Domains(domain_id.clone(), reply_sender)).await {
+            anyhow::bail!("Controller Channel Error: {}", err);
+        }
+        match reply_receiver.await {
+            Ok(ret) => ret,
+            Err(err) => Err(anyhow::anyhow!("Controller Channel Error: {}", err)),
         }
     }
 }
