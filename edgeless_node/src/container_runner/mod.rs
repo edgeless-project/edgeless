77900fb8901e67e53c386a391513cdf34313a45c
Spyridakis Christos <spyridakischristos@gmail.com>
2024-12-16 18:37:13 +0200

    Container runner updates to fix issue #226, related to functions execution in SGX

--- edgeless_node/src/container_runner/mod.rs
+++ edgeless_node/src/container_runner/mod.rs
@@ -1,6 +1,8 @@
 // SPDX-FileCopyrightText: © 2023 Claudio Cicconetti <c.cicconetti@iit.cnr.it>
 // SPDX-License-Identifier: MIT
 
+pub mod container_devices;
+
 pub mod container_runtime;
 
 pub mod docker_utils;
