96e5754b46988a3ef5cd0ce911ba419b4ff666f9
Francisco Vicente Parra <franvipa@gmail.com>
2025-01-30 14:23:14 +0100

    Cloud Offloading: First steps and tests with AWS SDK

---
+++ cloud-offloading/README.md
@@ -0,0 +1,49 @@
+# EDGELESS Cloud Offloading
+
+**EDGELESS Cloud Offloading** is an EDGELESS component responsible of the creation of EDGELESS nodes in the cloud for EDGELESS clusters. Built on top of the AWS SDK for Rust, this package provides a straightforward interface for managing cloud resources programmatically.
+
+## Features
+
+- Launch AWS EC2 instances with specified configurations.
+- Configure EC2 instances as EDGELESS nodes.
+- Terminate EC2 instances safely.
+
+## Prerequisites
+
+Before using the EDGELESS Cloud Offloading package, ensure you have the following:
+
+- An AWS account and appropriate permissions to create and manage EC2 instances.
+
+## AWS Credentials Configuration
+
+To interact with AWS services, you need to provide AWS credentials. Here are the recommended methods to configure these credentials securely:
+
+### 1. AWS Credentials File
+
+AWS CLI tools and SDKs look for credentials in the `~/.aws/credentials` file by default. You can set up your credentials like so:
+
+1. Open or create the file `~/.aws/credentials`.
+2. Add your credentials in the following format:
+
+    ```ini
+    [default]
+    aws_access_key_id = YOUR_ACCESS_KEY
+    aws_secret_access_key = YOUR_SECRET_KEY
+    ```
+
+### 2. Environment Variables
+
+You can also configure your credentials using environment variables. Set these in your shell or system environment:
+
+```bash
+export AWS_ACCESS_KEY_ID=YOUR_ACCESS_KEY
+export AWS_SECRET_ACCESS_KEY=YOUR_SECRET_KEY
+```
+
+## Configuration of AWS Region
+
+To specify the AWS region for your operations, you can use a `config.toml` file located in the root directory of this package. This file should look like the following:
+
+```toml
+[aws]
+region = "eu-west-1"
---
+++ cloud-offloading/src/main.rs
@@ -0,0 +1,76 @@
+use aws_sdk_ec2::types::Tag;
+use std::time::Duration;
+use tokio::time::sleep;
+use rand::{thread_rng, Rng};
+use rand::distributions::Alphanumeric;
+
+
+fn generate_instance_name() -> String {
+    let random_string: String = thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(8)
+        .map(char::from)
+        .collect();
+    format!("EDGELESS-Node-{}", random_string)
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+    // TODO: Get Region from config.toml file
+
+    let region_provider = "eu-west-1";
+    let config = aws_config::from_env().region(region_provider).load().await;
+
+    let client = aws_sdk_ec2::Client::new(&config);
+
+    // TODO: Create a Security Group for the instance
+
+    // Create an EC2 instance
+    let run_instances = client
+        .run_instances()
+        .image_id("ami-05edf2d87fdbd91c1") // TODO: Replace with a custom image for EDGELESS Node
+        .instance_type(aws_sdk_ec2::types::InstanceType::T2Micro) // TODO instance type should be an input
+        .min_count(1)
+        .max_count(1)
+        .send()
+        .await?;
+
+    if run_instances.instances().is_empty() {
+        println!("Failed to create instance");
+    }
+
+    let instance_id = run_instances.instances()[0].instance_id().unwrap();
+
+    // Define a name for the instance
+    let instance_name = generate_instance_name();
+    let response = client
+        .create_tags()
+        .resources(instance_id)
+        .tags(
+            Tag::builder()
+                .key("Name")
+                .value(&instance_name)
+                .build(),
+        )
+        .send()
+        .await?;
+
+    println!("Instance: {instance_id}, with name: {instance_name} has been created.");
+
+
+    // Pause to check the status
+    println!("Pausing for 120 seconds...");
+    sleep(Duration::from_secs(120)).await;
+
+    // Terminate the EC2 instance
+    client
+        .terminate_instances()
+        .instance_ids(instance_id)
+        .send()
+        .await?;
+
+    println!("Instance: {instance_id}, with name: {instance_name} has been terminated.");
+
+    Ok(())
+}
