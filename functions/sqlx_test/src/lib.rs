015b61a803acd00074d377bb6bc0cc33fc0bdc14
Claudio Cicconetti <c.cicconetti@iit.cnr.it>
2025-02-10 11:07:43 +0100

    sqlx_test function: add some wrappers to creare a generic template for sqlx functions

--- functions/sqlx_test/src/lib.rs
+++ functions/sqlx_test/src/lib.rs
@@ -1,13 +1,64 @@
 // SPDX-FileCopyrightText: © 2024 Chen Chen <cc2181@cam.ac.uk>
 // SPDX-License-Identifier: MIT
+
 use edgeless_function::*;
 
-struct Sqlx_test;
+struct SqlxTest;
 
-impl EdgeFunction for Sqlx_test {
-    fn handle_cast(_src: InstanceId, _message: &[u8]) {}
+#[derive(serde::Serialize, serde::Deserialize, Default)]
+struct WorkflowState {
+    id: String,
+    metadata: MyState,
+}
+
+#[derive(serde::Serialize, serde::Deserialize, Default)]
+struct MyState {
+    foo: f32,
+    bar: String,
+}
+
+fn call_wrapper(msg: &str) -> Option<WorkflowState> {
+    match call("database", msg.as_bytes()) {
+        CallRet::Reply(msg) => {
+            let reply = std::str::from_utf8(&msg).unwrap_or("not UTF8");
+            log::info!("Got Reply: {}", reply);
+            let cur_state: WorkflowState = serde_json::from_str(reply).unwrap_or_default();
+            Some(cur_state)
+        }
+        CallRet::NoReply => {
+            log::warn!("Received empty reply from the database");
+            None
+        }
+        CallRet::Err => {
+            log::error!("Error when calling the database");
+            None
+        }
+    }
+}
+
+impl EdgeFunction for SqlxTest {
+    fn handle_cast(_src: InstanceId, message: &[u8]) {
+        let new_value = core::str::from_utf8(message).unwrap().parse::<f32>().unwrap_or_default();
+
+        // Only update the state if an old one exists.
+        if let Some(old_state) = call_wrapper("SELECT id, metadata FROM WorkflowState WHERE id=$1") {
+            log::info!("wf_id {}, old value: {}, new value: {}", old_state.id, old_state.metadata.foo, new_value);
+            let new_state = MyState {
+                foo: new_value,
+                bar: String::from("subsequent"),
+            };
+            call_wrapper(
+                format!(
+                    "UPDATE WorkflowState SET metadata='{}'  WHERE id = $1",
+                    serde_json::to_string(&new_state).unwrap_or_default()
+                )
+                .as_str(),
+            );
+        }
+    }
 
     fn handle_call(_src: InstanceId, _message: &[u8]) -> CallRet {
+        log::warn!("Call method not supported");
         CallRet::NoReply
     }
 
@@ -15,31 +66,34 @@ impl EdgeFunction for Sqlx_test {
         edgeless_function::init_logger();
         log::info!("sqlx function init");
 
-        let res = call(
-            "database",
-            b"INSERT INTO WorkflowState (id, metadata) Values($1, '{\"foo\":1,\"bar\":\"first\"}')",
-        );
-        log::info!("call keep running");
-        call("database", b"SELECT id, metadata FROM WorkflowState WHERE id=$1");
-        call(
-            "database",
-            b"UPDATE WorkflowState SET metadata='{\"foo\":2,\"bar\":\"second\"}'  WHERE id = $1",
-        );
-        call("database", b"DELETE FROM WorkflowState WHERE id=$1");
+        let initial_value = core::str::from_utf8(init_message.unwrap_or_default())
+            .unwrap()
+            .parse::<f32>()
+            .unwrap_or_default();
 
-        //why is msg u8 not owneddatabyte?
-        if let CallRet::Reply(msg) = res {
-            if let Ok(msg) = std::str::from_utf8(&msg) {
-                log::info!("Got Reply: {}", msg);
-            } else {
-                log::info!("State management response not ok");
-            }
-        }
+        let initial_state = MyState {
+            foo: initial_value,
+            bar: String::from("initial"),
+        };
+
+        call_wrapper(
+            format!(
+                "INSERT INTO WorkflowState (id, metadata) Values($1, '{}')",
+                serde_json::to_string(&initial_state).unwrap_or_default()
+            )
+            .as_str(),
+        );
     }
 
     fn handle_stop() {
-        // sqlx
+        // Try to clean up state when leaving.
+        // Note: this operation fails if the database resource instance
+        // is terminated before this method is invoked (currently there is
+        // no way to enforce a specific sequence when terminating instances
+        // in a workflow).
+        log::info!("Try to clean database");
+        call_wrapper("DELETE FROM WorkflowState WHERE id=$1");
     }
 }
 
-edgeless_function::export!(Sqlx_test);
+edgeless_function::export!(SqlxTest);
