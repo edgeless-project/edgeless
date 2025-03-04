524d120b378a2c406013682db7373258840c279c
lukasz-zet <21034618+lukasz-zet@users.noreply.github.com>
2025-03-04 08:10:13 -0600

    fix: fix incomplete script, resolve merge issues

--- scripts/make-docker-compose.sh
+++ scripts/make-docker-compose.sh
@@ -18,12 +18,14 @@ if [ ! -r template-docker-compose.yml ] ; then
 fi
 
 if [ "$DDA" == "true" ] ; then
+    echo "generated template will contain a dedicated DDA node"
     OUT_FILE=docker-compose-$NUM_NODES+dda.yml
 else
+    echo "generated template will not contain a dedicated DDA node"
     OUT_FILE=docker-compose-$NUM_NODES.yml
 fi
-head -n 56 template-docker-compose.yml > $OUT_FILE
-sed -i '' "1 s/.*/\# generated through a tool, do not modify/" $OUT_FILE
+# TODO: what's the length now
+head -n 71 template-docker-compose.yml > $OUT_FILE
 
 # start the WASM nodes
 for (( i = 1 ; i <= $NUM_NODES ; i++ )) ; 
@@ -31,82 +33,83 @@ do
   port1=$(( 10003 + (i - 1) * 2 ))
   port2=$(( 10004 + (i - 1) * 2 ))
   label=${LABELS[ $RANDOM % ${#LABELS[@]} ]}
+  uuid=$(cat /proc/sys/kernel/random/uuid)
+  node_name=edgeless_node_$i
 
   cat >> $OUT_FILE << EOF
-  
+
   edgeless_node$i:
-<<<<<<< Updated upstream:deployment/make-docker-compose.sh
-    image: edgeless_node:latest
-||||||| Stash base:deployment/make-docker-compose.sh
-    image: edgeless_node:latest
-=======
     image: ghcr.io/edgeless-project/edgeless_node:v1.0.0
->>>>>>> Stashed changes:scripts/make-docker-compose.sh
-    container_name: edgeless_node$i
+    container_name: $node_name
     depends_on:
       - edgeless_orc
     environment:
       RUST_LOG: info
-      SHOWCONF: 1
-      INVOCATION_ENDPOINT: edgeless_node$i:$port1
-      AGENT_ENDPOINT: edgeless_node$i:$port2
-      NODE_REGISTER_URL: edgeless_orc:7004
-      LABELS: '["$label"]'
+      NODE_ID: $uuid
+      AGENT_HOST: 0.0.0.0
+      AGENT_PORT: 7005
+      AGENT_URL_ANNOUNCED: http://$node_name:7005
+      INVOCATION_HOST: 0.0.0.0
+      INVOCATION_PORT: 7002
+      INVOCATION_URL_ANNOUNCED: http://$node_name:7002
+      NODE_REGISTER_URL: http://edgeless_orc:7004 
+      SUBSCRIPTION_REFRESH_INTERVAL_SEC: 2
+      TELEMETRY_METRICS_HOST: 0.0.0.0
+      TELEMETRY_METRICS_PORT: 7003
+      TELEMETRY_LOG_LEVEL: info
+      TELEMETRY_PERFORMANCE_SAMPLES: true
+      WASM_RUNTIME_ENABLED: true
+      CONTAINER_RUNTIME_ENABLED: true
       NUM_CORES: $(( $RANDOM % 10 ))
-      NODE_TYPE: WASM
-    ports:
-      - $port1:$port1
-      - $port2:$port2
 EOF
 done
 
-# conditionally start DDA if an env variable is set to true. Also starts an mqtt
+# conditionally add a DDA if an env variable is set to true. Also add an mqtt
 # broker for that purpose.
+# TODO: adding dda requires the docker compose to be started from this dir, as
+# the dda/config.toml is available only here
 if [ "$DDA" == "true" ] ; then
-  echo "adding DDA node"
+  echo "adding a DDA node"
   port1=11003
   port2=11004
-  dda_port=12000
   label=${LABELS[ $RANDOM % ${#LABELS[@]} ]}
+  uuid=$(cat /proc/sys/kernel/random/uuid)
+  node_name=edgeless_node_dda
 
   cat >> $OUT_FILE << EOF
   
   edgeless_node_dda:
-<<<<<<< Updated upstream:deployment/make-docker-compose.sh
-    image: edgeless_node:latest
-    container_name: edgeless_node_dda
-||||||| Stash base:deployment/make-docker-compose.sh
-    image: edgeless_node:latest
-=======
     image: ghcr.io/edgeless-project/edgeless_node:v1.0.0
->>>>>>> Stashed changes:scripts/make-docker-compose.sh
+    container_name: $node_name
     depends_on:
       - edgeless_orc
     environment:
       RUST_LOG: info
-      SHOWCONF: 1
-      INVOCATION_ENDPOINT: edgeless_node_dda:$port1
-      AGENT_ENDPOINT: edgeless_node_dda:$port2
-      NODE_REGISTER_URL: edgeless_orc:7004
-      DDA_ENDPOINT: dda:$dda_port
-      LABELS: '["$label"]'
+      NODE_ID: $uuid
+      AGENT_HOST: 0.0.0.0
+      AGENT_PORT: 7005
+      AGENT_URL_ANNOUNCED: http://$node_name:7005
+      INVOCATION_HOST: 0.0.0.0
+      INVOCATION_PORT: 7002
+      INVOCATION_URL_ANNOUNCED: http://$node_name:7002
+      NODE_REGISTER_URL: http://edgeless_orc:7004 
+      SUBSCRIPTION_REFRESH_INTERVAL_SEC: 2
+      TELEMETRY_METRICS_HOST: 0.0.0.0
+      TELEMETRY_METRICS_PORT: 7003
+      TELEMETRY_LOG_LEVEL: info
+      TELEMETRY_PERFORMANCE_SAMPLES: true
+      WASM_RUNTIME_ENABLED: true
+      CONTAINER_RUNTIME_ENABLED: true
       NUM_CORES: $(( $RANDOM % 10 ))
-      NODE_TYPE: DDA
-    ports:
-      - $port1:$port1
-      - $port2:$port2
+      DDA_PROVIDER: dda-1
     
   dda:
     image: ghcr.io/coatyio/dda:latest
     volumes:
       - ./dda/:/dda 
-    ports:
-      - "12000:12000"
     
   mqtt_broker:
     image: eclipse-mosquitto:latest
     command: mosquitto -c /mosquitto-no-auth.conf
-    ports:
-      - "1883:1883"
 EOF
 fi
