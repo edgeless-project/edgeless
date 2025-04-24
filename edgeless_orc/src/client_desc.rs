4b166dd049904d28c54f941305cddd29a9f23d30
Claudio Cicconetti <c.cicconetti@iit.cnr.it>
2025-04-24 16:15:26 +0200

    edgeless_orc: reject nodes with empty host in invocation/agent URLs

--- edgeless_orc/src/client_desc.rs
+++ edgeless_orc/src/client_desc.rs
@@ -10,23 +10,33 @@ pub struct ClientDesc {
 
 impl ClientDesc {
     pub async fn from(request: &edgeless_api::node_registration::UpdateNodeRequest) -> anyhow::Result<Self> {
-        // Return immediately if the node has an invalid agent URL.
-        let (proto, host, port) = match edgeless_api::util::parse_http_host(&request.agent_url) {
-            Ok((proto, host, url)) => (proto, host, url),
-            Err(err) => {
-                anyhow::bail!("Invalid agent URL '{}' for node '{}': {}", request.agent_url, request.node_id, err);
-            }
-        };
+        // Return immediately if the node has an invalid agent or invocation URL.
+        let urls_to_validate = vec![("agent", &request.agent_url), ("invocation", &request.invocation_url)];
+        let mut proto_host_ports = vec![];
+        for (label, url) in urls_to_validate {
+            let (proto, host, port) = match edgeless_api::util::parse_http_host(url) {
+                Ok((proto, host, url)) => (proto, host, url),
+                Err(err) => {
+                    anyhow::bail!("Invalid {} URL '{}' for node '{}': {}", label, request.agent_url, request.node_id, err);
+                }
+            };
+            anyhow::ensure!(!host.is_empty(), "invalid empty host in {} URL", label);
+            proto_host_ports.push((proto, host, port));
+        }
+
+        let (proto, host, port) = proto_host_ports.first().unwrap();
 
         Ok(crate::client_desc::ClientDesc {
             agent_url: request.agent_url.clone(),
             invocation_url: request.invocation_url.clone(),
             api: match proto {
                 edgeless_api::util::Proto::COAP => {
-                    let addr = std::net::SocketAddrV4::new(host.parse().unwrap(), port);
+                    let addr = std::net::SocketAddrV4::new(host.parse().unwrap(), *port);
                     Box::new(edgeless_api::coap_impl::CoapClient::new(addr).await)
                 }
-                _ => Box::new(edgeless_api::grpc_impl::outer::agent::AgentAPIClient::new(&request.agent_url)),
+                edgeless_api::util::Proto::HTTP | edgeless_api::util::Proto::HTTPS => {
+                    Box::new(edgeless_api::grpc_impl::outer::agent::AgentAPIClient::new(&request.agent_url))
+                }
             },
             capabilities: request.capabilities.clone(),
         })
