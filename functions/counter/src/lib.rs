6a07b41734e91633812e46f5d0e7bf635c37b9bf
Claudio Cicconetti <c.cicconetti@iit.cnr.it>
2025-05-02 16:58:58 +0200

    Examples: remove state initialization from counter function (too complex, possibly misleading)

--- functions/counter/src/lib.rs
+++ functions/counter/src/lib.rs
@@ -9,7 +9,6 @@ impl EdgeFunction for Counter {
         let msg_str = core::str::from_utf8(message).unwrap();
         let prev_count = msg_str.parse::<i32>().unwrap();
         let cur_count = format!("{}", prev_count + 1);
-        cast("redis", cur_count.as_bytes());
         cast("output", cur_count.as_bytes());
         delayed_cast(1000, "self", cur_count.as_bytes());
     }
@@ -21,29 +20,16 @@ impl EdgeFunction for Counter {
     fn handle_init(init_message: Option<&[u8]>, _serialized_state: Option<&[u8]>) {
         edgeless_function::init_logger();
         let message = if let Some(init_message) = init_message {
-            match call("redis", "last_counter".as_bytes()) {
-                CallRet::Reply(owned_byte_buff) => {
-                    if let Ok(res) = core::str::from_utf8(&owned_byte_buff) {
-                        match res.parse::<i32>() {
-                            Ok(_) => res.to_string(),
-                            Err(_) => String::from("0"),
-                        }
-                    } else {
-                        String::from("0")
-                    }
-                }
-                CallRet::Err | CallRet::NoReply => {
-                    let init_msg_str = core::str::from_utf8(init_message).unwrap();
-                    match init_msg_str.parse::<i32>() {
-                        Ok(_) => init_msg_str.to_string(),
-                        Err(_) => String::from("0"),
-                    }
-                }
+            let init_msg_str = core::str::from_utf8(init_message).unwrap();
+            if init_msg_str.parse::<i32>().is_ok() {
+                init_message
+            } else {
+                b"0"
             }
         } else {
-            String::from("0")
+            b"0"
         };
-        cast("self", &message.as_bytes());
+        cast("self", message);
     }
 
     fn handle_stop() {
