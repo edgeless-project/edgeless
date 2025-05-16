18fa7f806f1af62e62583ef50f1f502d91b27744
Claudio Cicconetti <c.cicconetti@iit.cnr.it>
2025-05-16 16:02:37 +0200

    edgeless_node: refactor container resource provider into serverless resource provider

--- edgeless_node/src/bin/edgeless_node_d.rs
+++ edgeless_node/src/bin/edgeless_node_d.rs
@@ -14,6 +14,7 @@ use edgeless_node::resources::ollama::OllamaResourceSpec;
 use edgeless_node::resources::redis::RedisResourceSpec;
 use edgeless_node::resources::resource_provider_specs::ResourceProviderSpecOutput;
 use edgeless_node::resources::resource_provider_specs::ResourceProviderSpecs;
+use edgeless_node::resources::serverless::ServerlessResourceProviderSpec;
 
 #[derive(Debug, clap::Parser)]
 #[command(long_about = None)]
@@ -28,14 +29,30 @@ struct Args {
     output_json: bool,
 }
 
-fn main() -> anyhow::Result<()> {
-    env_logger::init();
+fn read_conf_from_file(filename: &str) -> anyhow::Result<edgeless_node::EdgelessNodeSettings> {
+    Ok(toml::from_str::<edgeless_node::EdgelessNodeSettings>(&std::fs::read_to_string(
+        filename,
+    )?)?)
+}
 
+fn main() -> anyhow::Result<()> {
     // NOTE: to debug the edgeless_node using the tokio-console, exchange the
     // env_logger::init() with the next line. These two options are exclusive.
+    env_logger::init();
     // console_subscriber::init();
 
     let args = Args::parse();
+
+    // Create a template node configuration and exit.
+    if !args.template.is_empty() {
+        edgeless_api::util::create_template(&args.template, edgeless_node::edgeless_node_default_conf().as_str())?;
+        return Ok(());
+    }
+
+    // Read the configuration file.
+    let conf = read_conf_from_file(&args.config_file);
+
+    // Print the available resources and exit.
     if args.available_resources {
         #[allow(unused_mut)]
         let mut specs: Vec<Box<dyn ResourceProviderSpecs>> = vec![
@@ -49,6 +66,15 @@ fn main() -> anyhow::Result<()> {
         ];
         #[cfg(feature = "rdkafka")]
         specs.push(Box::new(KafkaEgressResourceSpec {}));
+        if let Ok(conf) = &conf {
+            if let Some(resources) = &conf.resources {
+                if let Some(serverless_providers) = &resources.serverless_provider {
+                    for settings in serverless_providers {
+                        specs.push(Box::new(ServerlessResourceProviderSpec::new(&settings.class_type, &settings.version)))
+                    }
+                }
+            }
+        }
 
         if args.output_json {
             println!(
@@ -77,14 +103,9 @@ fn main() -> anyhow::Result<()> {
         }
         return Ok(());
     }
-    if !args.template.is_empty() {
-        edgeless_api::util::create_template(&args.template, edgeless_node::edgeless_node_default_conf().as_str())?;
-        return Ok(());
-    }
-    let conf: edgeless_node::EdgelessNodeSettings = toml::from_str(&std::fs::read_to_string(args.config_file)?)?;
 
     let async_runtime = tokio::runtime::Builder::new_multi_thread().worker_threads(8).enable_all().build()?;
-    let async_tasks = vec![async_runtime.spawn(edgeless_node::edgeless_node_main(conf.clone()))];
+    let async_tasks = vec![async_runtime.spawn(edgeless_node::edgeless_node_main(conf?))];
 
     async_runtime.block_on(async { futures::future::join_all(async_tasks).await });
     Ok(())
