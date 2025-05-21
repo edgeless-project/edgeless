c58344cd130327bfe61eb4ee3161112e853ca003
Francisco Vicente Parra <francisco.vicente@worldline.com>
2025-05-21 14:45:28 +0200

    Refactor cloud offloading implementation and rename it

---
+++ edgeless_cloud_offloading/src/lib.rs
@@ -0,0 +1,96 @@
+use aws_config::Region;
+use base64::{engine::general_purpose::STANDARD, Engine as _};
+use aws_sdk_ec2::types::{InstanceType, Tag};
+use rand::distributions::Alphanumeric;
+use rand::{thread_rng, Rng};
+
+pub struct CloudNodeInputData {
+    pub aws_region: String,
+    pub aws_ami_id: String,
+    pub aws_instance_type: String,
+    pub aws_security_group_id: String,
+    pub orchestrator_url: String
+}
+
+pub struct CloudNodeData {
+    aws_region: String,
+    instance_id: String,
+    instance_name: String
+}
+
+fn generate_instance_name() -> String {
+    let random_string: String = thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(8)
+        .map(char::from)
+        .collect();
+    format!("EDGELESS-Node-{}", random_string)
+}
+
+pub async fn create_cloud_node(input_data: CloudNodeInputData) -> Result<CloudNodeData, Box<dyn std::error::Error>> {
+
+    // Config for AWS SDK
+    let config = aws_config::from_env().region(Region::new(input_data.aws_region.clone())).load().await;
+    let client = aws_sdk_ec2::Client::new(&config);
+
+    // Get the user data script from the file and convert it to to Base64
+    const SCRIPT_CONTENT: &str = include_str!("ec2-user-data.sh");
+    let script_content_modified = SCRIPT_CONTENT.replace("__ORCHESTRATOR_URL_PLACEHOLDER__", &input_data.orchestrator_url);
+    let script_content_as_string = script_content_modified.to_string();
+    let encoded_user_data = STANDARD.encode(script_content_as_string);
+
+    // Create an EC2 instance
+    let run_instances = client
+        .run_instances()
+        .image_id(input_data.aws_ami_id)
+        .instance_type(InstanceType::from(input_data.aws_instance_type.as_str()))
+        .security_group_ids(input_data.aws_security_group_id)
+        .user_data(encoded_user_data)
+        .min_count(1)
+        .max_count(1)
+        .send()
+        .await?;
+
+    if run_instances.instances().is_empty() {
+        println!("Failed to create instance");
+        return Err(("Failed to create instance").into());
+    }
+
+    let instance_id = run_instances.instances()[0].instance_id().unwrap();
+
+    // Define a name for the instance
+    let instance_name = generate_instance_name();
+    let response = client
+        .create_tags()
+        .resources(instance_id)
+        .tags(Tag::builder().key("Name").value(&instance_name).build())
+        .send()
+        .await?;
+
+    println!("EDGELESS Node deployed on AWS Instance: {instance_id}, with name: {instance_name} has been created.");
+
+    // Build the CloudNode struct
+    let cloud_node = CloudNodeData {
+        aws_region: input_data.aws_region,
+        instance_id: instance_id.to_string(),
+        instance_name
+    };
+
+    Ok(cloud_node)
+}
+
+pub async fn delete_cloud_node(cloud_node: CloudNodeData) -> Result<String, Box<dyn std::error::Error>> {
+    let instance_id: &str = &cloud_node.instance_id;
+    let instance_name: &str = &cloud_node.instance_name;
+
+    // Config for AWS SDK
+    let config = aws_config::from_env().region(Region::new(cloud_node.aws_region)).load().await;
+    let client = aws_sdk_ec2::Client::new(&config);
+
+    // Terminate the EC2 instance
+    client.terminate_instances().instance_ids(instance_id).send().await?;
+
+    println!("EDGELESS Node deployed on AWS Instance: {instance_id}, with name: {instance_name} has been deleted.");
+
+    Ok(instance_id.to_string())
+}
---
+++ edgeless_cloud_offloading/src/main.rs
@@ -0,0 +1,31 @@
+use std::time::Duration;
+use tokio::time::sleep;
+
+use cloud_offloading::CloudNodeInputData;
+use cloud_offloading::create_cloud_node;
+use cloud_offloading::delete_cloud_node;
+
+#[tokio::main]
+// This is used for testing purposes only
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+    let cloud_input_data = CloudNodeInputData {
+        aws_region: "eu-west-1".to_string(),
+        aws_ami_id: "ami-035085b5449b0383a".to_string(),
+        aws_instance_type: "t2.medium".to_string(),
+        aws_security_group_id: "sg-09dcfc636643d2868".to_string(),
+        orchestrator_url: "3.253.97.217".to_string()
+    };
+
+    // Create a new EDGELESS Node in the cloud
+    let cloud_node = create_cloud_node(cloud_input_data).await?;
+
+    // Pause to check the status
+    println!("Pausing for 600 seconds...");
+    sleep(Duration::from_secs(600)).await;
+
+    // Delete the cloud node
+    delete_cloud_node(cloud_node).await?;
+
+    Ok(())
+}
