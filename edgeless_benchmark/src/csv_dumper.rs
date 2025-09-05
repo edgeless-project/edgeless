b8760c0808ac1cd2ffcbeff18382c3e88381ff37
Claudio Cicconetti <c.cicconetti@iit.cnr.it>
2025-09-05 08:54:36 +0000

    edgeless_benchmark: fix bug when running with an empty output file name

--- edgeless_benchmark/src/csv_dumper.rs
+++ edgeless_benchmark/src/csv_dumper.rs
@@ -6,52 +6,61 @@ use std::io::Write;
 
 pub struct CsvDumper {
     additional_fields: String,
-    outfile: std::fs::File,
+    outfile: Option<std::fs::File>,
 }
 
 impl CsvDumper {
     pub fn new(additional_fields: String, additional_header: String, filename: &str, append: bool) -> anyhow::Result<Self> {
-        let header = !append
-            || match std::fs::metadata(filename) {
-                Ok(metadata) => metadata.len() == 0,
-                Err(_) => true,
-            };
-
-        let mut outfile = std::fs::OpenOptions::new()
-            .write(true)
-            .append(append)
-            .create(true)
-            .truncate(!append)
-            .open(filename)?;
-
-        anyhow::ensure!(
-            additional_header.split(",").count() == additional_fields.split(",").count(),
-            "different number of comma-separated values in additional headers vs. fields"
-        );
-
-        let mut additional_header = additional_header;
         let mut additional_fields = additional_fields;
-        if !additional_header.is_empty() {
-            anyhow::ensure!(!additional_fields.is_empty(), "empty additional fields with non-empty additional header");
-            additional_header += ",";
-            additional_fields += ",";
-        }
-        if header {
-            writeln!(&mut outfile, "{}timestamp,metric,target,value", additional_header)?;
+        let mut outfile = None;
+        if !filename.is_empty() {
+            let header = !append
+                || match std::fs::metadata(filename) {
+                    Ok(metadata) => metadata.len() == 0,
+                    Err(_) => true,
+                };
+
+            outfile = Some(
+                std::fs::OpenOptions::new()
+                    .write(true)
+                    .append(append)
+                    .create(true)
+                    .truncate(!append)
+                    .open(filename)?,
+            );
+
+            let mut additional_header = additional_header;
+            anyhow::ensure!(
+                additional_header.split(",").count() == additional_fields.split(",").count(),
+                "different number of comma-separated values in additional headers vs. fields"
+            );
+
+            if !additional_header.is_empty() {
+                anyhow::ensure!(!additional_fields.is_empty(), "empty additional fields with non-empty additional header");
+                additional_header += ",";
+                additional_fields += ",";
+            }
+            if header {
+                if let Some(outfile) = &mut outfile {
+                    writeln!(outfile, "{}timestamp,metric,target,value", additional_header)?;
+                }
+            }
         }
 
         Ok(Self { additional_fields, outfile })
     }
 
     pub fn add(&mut self, metric: &str, target: &str, value: &str) {
-        let _ = writeln!(
-            self.outfile,
-            "{}{},{},{},{}",
-            self.additional_fields,
-            crate::utils::timestamp_now(),
-            metric,
-            target,
-            value
-        );
+        if let Some(outfile) = &mut self.outfile {
+            let _ = writeln!(
+                outfile,
+                "{}{},{},{},{}",
+                self.additional_fields,
+                crate::utils::timestamp_now(),
+                metric,
+                target,
+                value
+            );
+        }
     }
 }
