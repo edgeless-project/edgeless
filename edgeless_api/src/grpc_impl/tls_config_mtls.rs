7300629176e3df85576d5720af3b44c708d436d7
Claudio Cicconetti <c.cicconetti@iit.cnr.it>
2025-10-28 15:25:03 +0000

    Minor fix

--- edgeless_api/src/grpc_impl/tls_config_mtls.rs
+++ edgeless_api/src/grpc_impl/tls_config_mtls.rs
@@ -1,3 +1,4 @@
+use super::tls_config::TlsConfig;
 use anyhow::Result;
 use serde::Deserialize;
 use std::fs::File;
@@ -43,44 +44,6 @@ impl CombinedTlsConfig {
         file.read_to_string(&mut contents)?;
         let cfg: CombinedTlsConfig = toml::from_str(&contents)?;
         Ok(cfg)
-    }
-}
-
-#[derive(Debug, Deserialize, Clone)]
-pub struct TlsConfig {
-    /// Path to the Server's Cert
-    pub server_cert_path: Option<String>,
-    /// Path to the Server's private key
-    pub server_key_path: Option<String>,
-    /// Path to the Server's CA for Client verification
-    pub server_ca_path: Option<String>,
-
-    /// Path to the client's Cert
-    pub client_cert_path: Option<String>,
-    /// Path to the Client's private key
-    pub client_key_path: Option<String>,
-    /// Path to the client's CA for Server verification
-    pub client_ca_path: Option<String>,
-    /// TPM handle alternative for client private key
-    pub tpm_handle: Option<String>,
-    /// Optional domain name for TLS verification
-    pub domain_name: Option<String>,
-}
-
-impl Default for TlsConfig {
-    fn default() -> Self {
-        Self {
-            server_cert_path: None,
-            server_key_path: None,
-            server_ca_path: None,
-
-            client_cert_path: None,
-            client_key_path: None,
-            client_ca_path: None,
-
-            tpm_handle: None,
-            domain_name: Some("www.example.com".to_string()),
-        }
     }
 }
 
